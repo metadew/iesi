78099afe8220bb0d9795d0bcb84eb8d29494818a
Heliodore <francois.heliodore@accenture.com>
2019-04-08 17:06:53 +0200

    set up iesi rest project

---
+++ core/java/iesi-rest/src/main/java/io/metadew/iesi/server/rest/IesiRestApplication.java
@@ -0,0 +1,13 @@
+package io.metadew.iesi.server.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IesiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IesiRestApplication.class, args);
+	}
+
+}

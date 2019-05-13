563a58d3f4ce73bdd69f7c59231d1bcdcbbe5d33
metadew <36988432+metadew@users.noreply.github.com>
2019-05-13 11:33:18 +0200

    first version of java parser

---
+++ core/java/iesi-core/src/io/metadew/iesi/connection/java/operation/JarOperation.java
@@ -0,0 +1,46 @@
+package io.metadew.iesi.connection.java.operation;
+
+import java.io.File;
+import java.net.URL;
+import java.net.URLClassLoader;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import io.metadew.iesi.connection.java.tools.JarTools;
+import io.metadew.iesi.framework.execution.FrameworkExecution;
+import io.metadew.iesi.metadata.definition.JavaArchive;
+
+public class JarOperation {
+
+	private FrameworkExecution frameworkExecution;
+
+	public JarOperation() {
+	}
+
+	public JavaArchive getJavaArchiveDefinition(String fileName) {
+		JavaArchive javaArchive = null;
+		try {
+			File file = new File(fileName);
+			URLClassLoader urlClassLoader = new URLClassLoader(new URL[] { file.toURI().toURL() },
+					JarTools.class.getClassLoader());
+			
+			javaArchive = JarTools.getJavaArchiveDefinition(fileName, urlClassLoader);
+			ObjectMapper mapper = new ObjectMapper();
+			System.out.println(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(javaArchive));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return javaArchive;
+	}
+
+	// Getters and Setters
+	public FrameworkExecution getFrameworkExecution() {
+		return frameworkExecution;
+	}
+
+	public void setFrameworkExecution(FrameworkExecution frameworkExecution) {
+		this.frameworkExecution = frameworkExecution;
+	}
+
+}
---
+++ core/java/iesi-core/src/io/metadew/iesi/metadata/definition/JavaParameter.java
@@ -0,0 +1,40 @@
+package io.metadew.iesi.metadata.definition;
+
+public class JavaParameter {
+
+	private String name;
+	private String type;
+	private String typeClass;
+
+	// Constructors
+	public JavaParameter() {
+		
+	}
+
+	// Getters and Setters
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getTypeClass() {
+		return typeClass;
+	}
+
+	public void setTypeClass(String typeClass) {
+		this.typeClass = typeClass;
+	}
+
+
+}

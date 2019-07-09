6d60b311d0cf62caaa08235f3be568222e3f1df2
metadew <36988432+metadew@users.noreply.github.com>
2019-07-09 18:16:23 +0200

    fixes in line with lab1 prep

--- core/java/iesi-core/src/main/java/io/metadew/iesi/util/sql/SQLInsertStatements.java
+++ core/java/iesi-core/src/main/java/io/metadew/iesi/util/sql/SQLInsertStatements.java
@@ -4,11 +4,13 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 
+import io.metadew.iesi.connection.tools.FileTools;
+
 public class SQLInsertStatements {
 
 	public static void main(String[] args) {
 
-		String inputFile = "C:/Data/file.csv";
+		String inputFile = "C:/Data/customers20190705.csv";
 		boolean headers = true;
 		try {
 			@SuppressWarnings("resource")
@@ -17,7 +19,7 @@ public class SQLInsertStatements {
 			int i = 0;
 			StringBuilder insertStart = new StringBuilder();
 			insertStart.append("INSERT INTO ");
-			insertStart.append("customers");
+			insertStart.append("#team#.customers");
 			insertStart.append(" (");
 			while ((readLine = bufferedReader.readLine()) != null) {
 				if (headers) {
@@ -39,13 +41,32 @@ public class SQLInsertStatements {
 			        String[] values = readLine.split(";");
 			        for (int j = 0; j < values.length; j++) {
 			        	if (j > 0) insertStart.append(",");
-			        	insertStart.append(values[j]);				        	
+			        	
+			        	// customers
+			        	if (j == 0) insertStart.append(values[j]);
+			        	if (j == 1) insertStart.append("'").append(values[j]).append("'");
+			        	if (j == 2) insertStart.append("'").append(values[j]).append("'");
+			        	if (j == 3) insertStart.append("'").append(values[j]).append("'");
+			        	if (j == 4) insertStart.append("'").append(values[j]).append("'");
+			        	if (j == 5) insertStart.append("'").append(values[j]).append("'");
+			        	if (j == 6) insertStart.append("'").append(values[j]).append("'");
+			        	if (j == 7) insertStart.append("'").append(values[j]).append("'");
+			        	if (j == 8) insertStart.append("'").append(values[j]).append("'");
+			        	if (j == 9) insertStart.append("'").append(values[j]).append("'");
+			        	if (j == 10) insertStart.append("'").append(values[j]).append("'");
+			        	
+			        	//insertStart.append(values[j]);				        	
 			        }
 				}
 				
 				insertStart.append(");");
 		        System.out.println(insertStart.toString());
 				System.out.println(readLine);
+				
+				String filePath = "c:/Data/customers.dml";
+				
+				FileTools.delete(filePath);
+				FileTools.appendToFile(filePath, "", insertStart.toString());
 				
 				// TODO mapping to data type
 				i++;

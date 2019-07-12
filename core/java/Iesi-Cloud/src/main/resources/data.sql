53ca9f49e4a7130ce6b69b2df8821490f48996ea
Robbe Berrevoets <6872974+BerrevoetsRobbe@users.noreply.github.com>
2019-07-12 10:07:52 +0200

    project clean up

--- core/java/Iesi-Cloud/src/main/resources/data.sql
+++ core/java/Iesi-Cloud/src/main/resources/data.sql
@@ -1,8 +1,8 @@
-INSERT INTO users (username,password,enabled) 
-    VALUES ('admin','$2a$10$dJYXnn2ztBU72ooXSyl9.u/xF29ADNp3mbQYy/6cvkiGLskNG1UQi', TRUE);
+--INSERT INTO users (username,password,enabled)
+--    VALUES ('admin','$2a$10$dJYXnn2ztBU72ooXSyl9.u/xF29ADNp3mbQYy/6cvkiGLskNG1UQi', TRUE);
 --    e9:wVS;f*[]QFp42/3yA&'Q+73)/Gn4? $2a$10$5Abn80LM.au6d7PWmy5KpuWgeTE7qhnWPbnsKLsfJxjEidmN4YfzO
 
-INSERT INTO groups (id, group_name) VALUES (1, 'USER_GROUP');
-INSERT INTO groups (id, group_name) VALUES (2, 'ADMIN_GROUP');
+--INSERT INTO groups (id, group_name) VALUES (1, 'USER_GROUP');
+--INSERT INTO groups (id, group_name) VALUES (2, 'ADMIN_GROUP');
 
-INSERT INTO authorities (group_id, authority, username) VALUES (2, 'AUTHORIZED_ADMIN','admin');
+--INSERT INTO authorities (group_id, authority, username) VALUES (2, 'AUTHORIZED_ADMIN','admin');
--- core/java/iesi-rest-with-microservices/src/test/java/io/metadew/iesi/server/rest/IesiRestApplicationTests.java
+++ core/java/iesi-rest-with-microservices/src/test/java/io/metadew/iesi/server/rest/IesiRestApplicationTests.java
@@ -1,116 +1,116 @@
-package io.metadew.iesi.server.rest;
-
-import io.metadew.iesi.metadata.configuration.ConnectionConfiguration;
-import io.metadew.iesi.metadata.definition.Connection;
-import io.metadew.iesi.metadata.definition.ConnectionParameter;
-import io.metadew.iesi.server.rest.controller.ConnectionsController;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-
-import java.util.Collections;
-import java.util.List;
-
-import static org.hamcrest.Matchers.hasSize;
-import static org.hamcrest.Matchers.is;
-import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-@RunWith(SpringRunner.class)
-@WebMvcTest(ConnectionsController.class)
-public class IesiRestApplicationTests {
-
-//	@TestConfiguration
-//	static class EmployeeServiceImplTestContextConfiguration {
+//package io.metadew.iesi.server.rest;
 //
-//		@Bean
-//		public FrameworkExecution frameworkExecution() {
+//import io.metadew.iesi.metadata.configuration.ConnectionConfiguration;
+//import io.metadew.iesi.metadata.definition.Connection;
+//import io.metadew.iesi.metadata.definition.ConnectionParameter;
+//import io.metadew.iesi.server.rest.controller.ConnectionsController;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+//import org.springframework.boot.test.mock.mockito.MockBean;
+//import org.springframework.test.context.junit4.SpringRunner;
+//import org.springframework.test.web.servlet.MockMvc;
+//
+//import java.util.Collections;
+//import java.util.List;
+//
+//import static org.hamcrest.Matchers.hasSize;
+//import static org.hamcrest.Matchers.is;
+//import static org.mockito.BDDMockito.given;
+//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+//
+//@RunWith(SpringRunner.class)
+//@WebMvcTest(ConnectionsController.class)
+//public class IesiRestApplicationTests {
+//
+////	@TestConfiguration
+////	static class EmployeeServiceImplTestContextConfiguration {
+////
+////		@Bean
+////		public FrameworkExecution frameworkExecution() {
+//////			Context context = new Context();
+//////			context.setName("restserver");
+//////			context.setScope("");
+//////			FrameworkExecutionContext frameworkExecutionContext = new FrameworkExecutionContext(context);
+//////
+//////			FrameworkInitializationFile frameworkInitializationFile = new FrameworkInitializationFile();
+//////			String frameworkHome = "C:\\Users\\robbe.berrevoets\\git\\iesi\\core\\java\\iesi-rest\\src\\test\\resources\\iesi_instance";
+//////			FrameworkFolderConfiguration frameworkFolderConfiguration = new FrameworkFolderConfiguration(frameworkHome);
+//////			FrameworkSettingConfiguration frameworkSettingConfiguration = new FrameworkSettingConfiguration(frameworkHome);
+//////			FrameworkActionTypeConfiguration frameworkActionTypeConfiguration = new FrameworkActionTypeConfiguration(frameworkFolderConfiguration);
+//////			FrameworkGenerationRuleTypeConfiguration frameworkGenerationRuleTypeConfiguration = new FrameworkGenerationRuleTypeConfiguration(frameworkFolderConfiguration);
+//////
+//////			FrameworkConfiguration frameworkConfiguration = new FrameworkConfiguration(frameworkHome, frameworkFolderConfiguration,
+//////					frameworkSettingConfiguration, frameworkActionTypeConfiguration, frameworkGenerationRuleTypeConfiguration);
+//////
+//////			return new FrameworkExecution(new FrameworkInstance("write", frameworkInitializationFile, frameworkConfiguration),
+//////					frameworkExecutionContext, frameworkInitializationFile);
+////
 ////			Context context = new Context();
 ////			context.setName("restserver");
 ////			context.setScope("");
 ////			FrameworkExecutionContext frameworkExecutionContext = new FrameworkExecutionContext(context);
-////
 ////			FrameworkInitializationFile frameworkInitializationFile = new FrameworkInitializationFile();
-////			String frameworkHome = "C:\\Users\\robbe.berrevoets\\git\\iesi\\core\\java\\iesi-rest\\src\\test\\resources\\iesi_instance";
-////			FrameworkFolderConfiguration frameworkFolderConfiguration = new FrameworkFolderConfiguration(frameworkHome);
-////			FrameworkSettingConfiguration frameworkSettingConfiguration = new FrameworkSettingConfiguration(frameworkHome);
-////			FrameworkActionTypeConfiguration frameworkActionTypeConfiguration = new FrameworkActionTypeConfiguration(frameworkFolderConfiguration);
-////			FrameworkGenerationRuleTypeConfiguration frameworkGenerationRuleTypeConfiguration = new FrameworkGenerationRuleTypeConfiguration(frameworkFolderConfiguration);
+////			frameworkInitializationFile.setName("");
+////			return new FrameworkExecution(new FrameworkInstance(frameworkInitializationFile), frameworkExecutionContext, frameworkInitializationFile);
 ////
-////			FrameworkConfiguration frameworkConfiguration = new FrameworkConfiguration(frameworkHome, frameworkFolderConfiguration,
-////					frameworkSettingConfiguration, frameworkActionTypeConfiguration, frameworkGenerationRuleTypeConfiguration);
+////		}
 ////
-////			return new FrameworkExecution(new FrameworkInstance("write", frameworkInitializationFile, frameworkConfiguration),
-////					frameworkExecutionContext, frameworkInitializationFile);
+////		@Bean
+////		public ConnectionConfiguration connectionConfiguration(FrameworkExecution frameworkExecution) {
+////			return new ConnectionConfiguration(frameworkExecution);
+////		}
+////
+////		@Bean
+////		public EnvironmentConfiguration environmentConfiguration(FrameworkExecution frameworkExecution) {
+////			return new EnvironmentConfiguration(frameworkExecution);
+////		}
+////
+////		@Bean
+////		public ImpersonationConfiguration impersonationConfiguration(FrameworkExecution frameworkExecution) {
+////			return new ImpersonationConfiguration(frameworkExecution);
+////		}
+////
+////		@Bean
+////		public ScriptConfiguration scriptConfiguration(FrameworkExecution frameworkExecution) {
+////			return new ScriptConfiguration(frameworkExecution);
+////		}
+////
+////		@Bean
+////		public UserConfiguration userConfiguration(FrameworkExecution frameworkExecution) {
+////			return new UserConfiguration(frameworkExecution);
+////		}
+////
+////		@Bean
+////		public ComponentConfiguration componentConfiguration(FrameworkExecution frameworkExecution) {
+////			return new ComponentConfiguration(frameworkExecution);
+////		}
+////	}
 //
-//			Context context = new Context();
-//			context.setName("restserver");
-//			context.setScope("");
-//			FrameworkExecutionContext frameworkExecutionContext = new FrameworkExecutionContext(context);
-//			FrameworkInitializationFile frameworkInitializationFile = new FrameworkInitializationFile();
-//			frameworkInitializationFile.setName("");
-//			return new FrameworkExecution(new FrameworkInstance(frameworkInitializationFile), frameworkExecutionContext, frameworkInitializationFile);
 //
-//		}
+//	@MockBean
+//	private ConnectionConfiguration connectionConfiguration;
 //
-//		@Bean
-//		public ConnectionConfiguration connectionConfiguration(FrameworkExecution frameworkExecution) {
-//			return new ConnectionConfiguration(frameworkExecution);
-//		}
+//	@Autowired
+//	private MockMvc mvc;
 //
-//		@Bean
-//		public EnvironmentConfiguration environmentConfiguration(FrameworkExecution frameworkExecution) {
-//			return new EnvironmentConfiguration(frameworkExecution);
-//		}
+//	@Test
+//	public void contextLoads() throws Exception {
+//		Connection connection = new Connection("test", "db.test", "description", "environment",
+//				Collections.singletonList(new ConnectionParameter("param1", "value1")));
+//		List<Connection> components = Collections.singletonList(connection);
 //
-//		@Bean
-//		public ImpersonationConfiguration impersonationConfiguration(FrameworkExecution frameworkExecution) {
-//			return new ImpersonationConfiguration(frameworkExecution);
-//		}
+//		given(connectionConfiguration.getConnections()).willReturn(components);
 //
-//		@Bean
-//		public ScriptConfiguration scriptConfiguration(FrameworkExecution frameworkExecution) {
-//			return new ScriptConfiguration(frameworkExecution);
-//		}
-//
-//		@Bean
-//		public UserConfiguration userConfiguration(FrameworkExecution frameworkExecution) {
-//			return new UserConfiguration(frameworkExecution);
-//		}
-//
-//		@Bean
-//		public ComponentConfiguration componentConfiguration(FrameworkExecution frameworkExecution) {
-//			return new ComponentConfiguration(frameworkExecution);
-//		}
+//		mvc.perform(get("/api/connections"))
+//				.andExpect(status().isOk())
+//				.andExpect(jsonPath("$._embedded").isArray())
+//				.andExpect(jsonPath("$._embedded", hasSize(1)))
+//				.andExpect(jsonPath("$._embedded[0].name", is("test")))
+//				.andExpect(jsonPath("$._embedded[0].type", is("db.test")));
 //	}
-
-
-	@MockBean
-	private ConnectionConfiguration connectionConfiguration;
-
-	@Autowired
-	private MockMvc mvc;
-
-	@Test
-	public void contextLoads() throws Exception {
-		Connection connection = new Connection("test", "db.test", "description", "environment",
-				Collections.singletonList(new ConnectionParameter("param1", "value1")));
-		List<Connection> components = Collections.singletonList(connection);
-
-		given(connectionConfiguration.getConnections()).willReturn(components);
-
-		mvc.perform(get("/api/connections"))
-				.andExpect(status().isOk())
-				.andExpect(jsonPath("$._embedded").isArray())
-				.andExpect(jsonPath("$._embedded", hasSize(1)))
-				.andExpect(jsonPath("$._embedded[0].name", is("test")))
-				.andExpect(jsonPath("$._embedded[0].type", is("db.test")));
-	}
-
-}
+//
+//}

4475c8caacfa41f863af707df428612bb3d6b588
Robbe Berrevoets <6872974+BerrevoetsRobbe@users.noreply.github.com>
2020-01-13 16:38:53 +0100

    Update core/java/iesi-core/src/main/java/io/metadew/iesi/metadata/configuration/script/trace/ScriptParameterTraceConfiguration.java

--- core/java/iesi-core/src/main/java/io/metadew/iesi/metadata/configuration/script/trace/ScriptParameterTraceConfiguration.java
+++ core/java/iesi-core/src/main/java/io/metadew/iesi/metadata/configuration/script/trace/ScriptParameterTraceConfiguration.java
@@ -122,6 +122,6 @@ public class ScriptParameterTraceConfiguration extends Configuration<ScriptParam
                 " SET SCRIPT_PAR_VAL = " + SQLTools.GetStringForSQL(scriptParameterTrace.getScriptParameterValue()) +
                 " WHERE RUN_ID = " + SQLTools.GetStringForSQL(scriptParameterTrace.getMetadataKey().getRunId()) +
                 "AND PRC_ID = " + SQLTools.GetStringForSQL(scriptParameterTrace.getMetadataKey().getProcessId()) +
-                "AND SCRIPT_PAR_NM = " + SQLTools.GetStringForSQL(scriptParameterTrace.getMetadataKey().getScriptParameterName()) + ";";
+                " AND SCRIPT_PAR_NM = " + SQLTools.GetStringForSQL(scriptParameterTrace.getMetadataKey().getScriptParameterName()) + ";";
     }
 }

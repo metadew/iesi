93ede74336fab1e7d7d9f184af758cb38eaf5a89
Brice Bartoletti <bartoletti.brice@accenture.com>
2020-06-15 15:00:04 +0200

    Refactor to assertJ + introduce more test

--- core/java/iesi-core/src/test/java/io/metadew/iesi/script/execution/instruction/data/number/NumberBetweenTest.java
+++ core/java/iesi-core/src/test/java/io/metadew/iesi/script/execution/instruction/data/number/NumberBetweenTest.java
@@ -1,53 +1,297 @@
 package io.metadew.iesi.script.execution.instruction.data.number;
 
+import io.metadew.iesi.datatypes.array.Array;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class NumberBetweenTest {
 
     NumberBetween numberBetween = new NumberBetween();
 
     @Test
-    void illegalInput() {
-        assertAll(()->{
-            assertThrows(IllegalArgumentException.class, () -> numberBetween.generateOutput(""), "parameters : \"\"");
-            assertThrows(IllegalArgumentException.class, () -> numberBetween.generateOutput(" Illegal, 5"), "parameters: \" Illegal, 5\"");
-            assertThrows(IllegalArgumentException.class, () -> numberBetween.generateOutput(" 5, Illegal"), "parameters: \" 5, Illegal\"");
-            assertThrows(IllegalArgumentException.class, () -> numberBetween.generateOutput(" Illegal, Illegal"), "parameters: \" Illegal, Illegal\"");
-            assertThrows(IllegalArgumentException.class, () -> numberBetween.generateOutput(" Illegal, 5, 32"), "parameters: \" Illegal, 5, 32\"");
-            assertThrows(IllegalArgumentException.class, () -> numberBetween.generateOutput(" 2  , 5 , -1"), "parameters: \" 2  , 5 , -1\"");
-            assertThrows(IllegalArgumentException.class, () -> numberBetween.generateOutput("   , 5 , 1"), "parameters: \"   , 5 , 1\"");
-            assertThrows(IllegalArgumentException.class, () -> numberBetween.generateOutput(" 5  ,  , 1"), "parameters: \" 5  ,  , 1\"");
-            assertThrows(IllegalArgumentException.class, () -> numberBetween.generateOutput(" 5  , 2 ,  "),"parameters: \" 5  , 2 ,  \"");
-        });
+    void illegalInputTest() {
+
+        assertThatIllegalArgumentException()
+                .as("empty parameters should throw an IllegalArgumentException")
+                .isThrownBy(() -> numberBetween.generateOutput(""));
+
+        assertThatIllegalArgumentException()
+                .as("Illegal first argument should throw an IllegalArgumentException")
+                .isThrownBy(() -> numberBetween.generateOutput(" Illegal, 5"));
+
+        assertThatIllegalArgumentException()
+                .as("Illegal second argument should throw an IllegalArgumentException")
+                .isThrownBy(() -> numberBetween.generateOutput(" 5, Illegal"));
+
+        assertThatIllegalArgumentException()
+                .as("2 illegals arguments should throw an IllegalArgumentException")
+                .isThrownBy(() -> numberBetween.generateOutput(" Illegal, Illegal"));
+
+        assertThatIllegalArgumentException()
+                .as("Illegal first argument should throw an IllegalArgumentException")
+                .isThrownBy(() -> numberBetween.generateOutput(" Illegal, 5, 32"));
+
+        assertThatIllegalArgumentException()
+                .as("Negative 3rd argument is Illegal and should throw an IllegalArgumentException")
+                .isThrownBy(() -> numberBetween.generateOutput(" 2  , 5 , -1"));
+
+        assertThatIllegalArgumentException()
+                .as("Empty first argument is Illegal and should throw an IllegalArgumentException")
+                .isThrownBy(() -> numberBetween.generateOutput("   , 5 , 1"));
+
+        assertThatIllegalArgumentException()
+                .as("Empty second argument is Illegal and should throw an IllegalArgumentException")
+                .isThrownBy(() -> numberBetween.generateOutput(" 5  ,  , 1"));
+
+        assertThatIllegalArgumentException()
+                .as("Optional parameter initiated and left empty is an illegal argument and should throw an IllegalArgumentException")
+                .isThrownBy(() -> numberBetween.generateOutput(" 5  , 2 ,  "));
+
+        assertThatIllegalArgumentException()
+                .as("3rd argument as a float is an illegal argument and should throw an IllegalArgumentException")
+                .isThrownBy(() -> numberBetween.generateOutput(" 1  , 2 , 2.0 "));
+
+        assertThatIllegalArgumentException()
+                .as("3rd argument as a float is an illegal argument and should throw an IllegalArgumentException")
+                .isThrownBy(() -> numberBetween.generateOutput(" 1  , 2 , 3.58 "));
+
+        assertThatIllegalArgumentException()
+                .as("\"--\" is illegal an should throw an IllegalArgumentException")
+                .isThrownBy(() -> numberBetween.generateOutput(" --1  , 2"));
+
+        assertThatIllegalArgumentException()
+                .as("\"--\" is illegal an should throw an IllegalArgumentException")
+                .isThrownBy(() -> numberBetween.generateOutput(" 1  , --2"));
+
     }
 
     @Test
-    void shortNumberFormatTests() {
-        assertAll(()->{
-            assertEquals("2", numberBetween.generateOutput("2,2,0"),"parameters: 2,2,0");
-            assertEquals("2.0", numberBetween.generateOutput("2,2,1"),"parameters: 2,2,1");
-            assertEquals("2.0", numberBetween.generateOutput("2,2"),"parameters: 2,2");
-            assertEquals("2.00", numberBetween.generateOutput("2,2,2"),"parameters: 2,2,2");
-            assertEquals("2.000", numberBetween.generateOutput("2,2,3"),"parameters: 2,2,3");
-            assertEquals("2.0000000000", numberBetween.generateOutput("2,2,10"),"parameters: 2,2,10");
-        });
+    void shortNumberFormatTest() {
+        assertThat(numberBetween.generateOutput("2,2"))
+                .as("parameters: 2,2")
+                .isEqualTo("2.0");
+
+        assertThat(numberBetween.generateOutput("2,2,0"))
+                .as("parameters: 2,2,0")
+                .isEqualTo("2");
+
+        assertThat(numberBetween.generateOutput("2,2,1"))
+                .as("parameters: 2,2,1")
+                .isEqualTo("2.0");
+
+        assertThat(numberBetween.generateOutput("2,2,2"))
+                .as("parameters: 2,2,2")
+                .isEqualTo("2.00");
+
+        assertThat(numberBetween.generateOutput("2,2,3"))
+                .as("parameters: 2,2,3")
+                .isEqualTo("2.000");
+
+        assertThat(numberBetween.generateOutput("2,2,10"))
+                .as("parameters: 2,2,1")
+                .isEqualTo("2.0000000000");
+
     }
 
     @Test
-    void biggerNumberFormatTest(){
-        assertEquals("2000.0" , numberBetween.generateOutput("2000,2000"), "parameter: 2000,2000");
-        assertEquals("2000" , numberBetween.generateOutput("2000,2000,0"), "parameter: 2000,2000,0");
-        assertEquals("2000.0" , numberBetween.generateOutput("2000,2000,1"), "parameter: 2000,2000,1");
-        assertEquals("2000.00" , numberBetween.generateOutput("2000,2000,2"), "parameter: 2000,2000,2");
+    void shortNegativeNumberFormatTest() {
+        assertThat(numberBetween.generateOutput("-2,-2"))
+                .as("parameters: -2,-2")
+                .isEqualTo("-2.0");
+
+        assertThat(numberBetween.generateOutput("-2,-2,0"))
+                .as("parameters: -2,-2,0")
+                .isEqualTo("-2");
+
+        assertThat(numberBetween.generateOutput("-2,-2,1"))
+                .as("parameters: -2,-2,1")
+                .isEqualTo("-2.0");
+
+        assertThat(numberBetween.generateOutput("-2,-2,2"))
+                .as("parameters: -2,-2,2")
+                .isEqualTo("-2.00");
+
+        assertThat(numberBetween.generateOutput("-2,-2,3"))
+                .as("parameters: -2,-2,3")
+                .isEqualTo("-2.000");
+
+        assertThat(numberBetween.generateOutput("-2,-2,10"))
+                .as("parameters: -2,-2,1")
+                .isEqualTo("-2.0000000000");
+
     }
 
     @Test
-    void floatNumberFormatTest(){
-        assertEquals("1999.99",numberBetween.generateOutput("1999.99,1999.99,2"));
-        assertEquals("1999.99879",numberBetween.generateOutput("1999.99879,1999.99879,5"));
-        assertEquals("2000.00",numberBetween.generateOutput("1999.99879,1999.99879,2"));
+    void biggerNumberFormatTest() {
+        assertThat(numberBetween.generateOutput("200000,200000"))
+                .as("parameter: 200000,200000")
+                .isEqualTo("200000.0");
+
+        assertThat(numberBetween.generateOutput("200000,200000,0"))
+                .as("parameter: 200000,200000,0")
+                .isEqualTo("200000");
+
+        assertThat(numberBetween.generateOutput("200000,200000,1"))
+                .as("parameter: 200000,200000,1")
+                .isEqualTo("200000.0");
+
+        assertThat(numberBetween.generateOutput("200000,200000,2"))
+                .as("parameter: 200000,200000,2")
+                .isEqualTo("200000.00");
+
+        assertThat(numberBetween.generateOutput("200000,200000,3"))
+                .as("parameter: 200000,200000,2")
+                .isEqualTo("200000.000");
+
+        assertThat(numberBetween.generateOutput("200000,200000,4"))
+                .as("parameter: 200000,200000,2")
+                .isEqualTo("200000.0000");
+
+        assertThat(numberBetween.generateOutput("200000,200000,5"))
+                .as("parameter: 200000,200000,2")
+                .isEqualTo("200000.00000");
+
+    }
+
+    @Test
+    void floatNumberFormatTest() {
+        assertThat(numberBetween.generateOutput("1999.99,1999.99,2"))
+                .isEqualTo("1999.99");
+
+        assertThat(numberBetween.generateOutput("1999.99879,1999.99879,5"))
+                .isEqualTo("1999.99879");
+
+        assertThat(numberBetween.generateOutput("1999.99879,1999.99879,2"))
+                .isEqualTo("2000.00");
+
+    }
+
+    @Test
+    void outputRangeTest2args() {
+        assertThat(Double.parseDouble(numberBetween.generateOutput("0, 10")))
+                .isNotNull()
+                .isNotNegative()
+                .isBetween(0.0, 10.0);
+
+        assertThat(Double.parseDouble(numberBetween.generateOutput("0.0, 10.0")))
+                .isNotNull()
+                .isNotNegative()
+                .isBetween(0.0, 10.0);
+
+        assertThat(Double.parseDouble(numberBetween.generateOutput("0.0, 100.0")))
+                .isNotNull()
+                .isNotNegative()
+                .isBetween(0.0, 100.0);
+
+        assertThat(Double.parseDouble(numberBetween.generateOutput("0.0, 1000.0")))
+                .isNotNull()
+                .isNotNegative()
+                .isBetween(0.0, 1000.0);
+
+        assertThat(Double.parseDouble(numberBetween.generateOutput("0.0, 10000.0")))
+                .isNotNull()
+                .isNotNegative()
+                .isBetween(0.0, 10000.0);
+
+    }
+
+    @Test
+    void outputRangeTest3rdArgs() {
+
+        assertThat(Double.parseDouble(numberBetween.generateOutput("0, 1000, 0")))
+                .isNotNull()
+                .isNotNegative()
+                .isBetween(0.0, 1000.0);
+
+        assertThat(Double.parseDouble(numberBetween.generateOutput("0, 10, 0")))
+                .isNotNull()
+                .isNotNegative()
+                .isBetween(0.0, 10.0);
+
+        assertThat(Double.parseDouble(numberBetween.generateOutput("0.0, 1000.0, 1")))
+                .isNotNull()
+                .isNotNegative()
+                .isBetween(0.0, 1000.0);
+
+        assertThat(Double.parseDouble(numberBetween.generateOutput("0.0, 10.0, 1")))
+                .isNotNull()
+                .isNotNegative()
+                .isBetween(0.0, 10.0);
+
+        assertThat(Double.parseDouble(numberBetween.generateOutput("0.0, 1000.0, 2")))
+                .isNotNull()
+                .isNotNegative()
+                .isBetween(0.0, 1000.0);
+
+        assertThat(Double.parseDouble(numberBetween.generateOutput("0.0, 10.0, 2")))
+                .isNotNull()
+                .isNotNegative()
+                .isBetween(0.0, 10.0);
+
+        assertThat(Double.parseDouble(numberBetween.generateOutput("0.0, 1000.0, 3")))
+                .isNotNull()
+                .isNotNegative()
+                .isBetween(0.0, 1000.0);
+
+        assertThat(Double.parseDouble(numberBetween.generateOutput("0.0, 10.0, 3")))
+                .isNotNull()
+                .isNotNegative()
+                .isBetween(0.0, 10.0);
+
+        assertThat(Double.parseDouble(numberBetween.generateOutput("0.0, 1000.0, 4")))
+                .isNotNull()
+                .isNotNegative()
+                .isBetween(0.0, 1000.0);
+
+        assertThat(Double.parseDouble(numberBetween.generateOutput("0.0, 10.0, 4")))
+                .isNotNull()
+                .isNotNegative()
+                .isBetween(0.0, 10.0);
+
+        assertThat(Double.parseDouble(numberBetween.generateOutput("0.0, 1000.0, 5")))
+                .isNotNull()
+                .isNotNegative()
+                .isBetween(0.0, 1000.0);
+
+        assertThat(Double.parseDouble(numberBetween.generateOutput("0.0, 10.0, 5")))
+                .isNotNull()
+                .isNotNegative()
+                .isBetween(0.0, 10.0);
+
+    }
+
+    @Test
+    void negativeNumberTest() {
+        assertThat(Double.parseDouble(numberBetween.generateOutput("-10, -1")))
+                .isNotNull()
+                .isNegative()
+                .isBetween(-10.00, -1.0);
+
+        assertThat(Double.parseDouble(numberBetween.generateOutput("-10, -1, 0")))
+                .isNotNull()
+                .isNegative()
+                .isBetween(-10.00, -1.0);
+
+        assertThat(Double.parseDouble(numberBetween.generateOutput("-10, -1, 1")))
+                .isNotNull()
+                .isNegative()
+                .isBetween(-10.00, -1.0);
+
+        assertThat(Double.parseDouble(numberBetween.generateOutput("-10, -1, 2")))
+                .isNotNull()
+                .isNegative()
+                .isBetween(-10.00, -1.0);
+
+        assertThat(Double.parseDouble(numberBetween.generateOutput("-10, -1, 3")))
+                .isNotNull()
+                .isNegative()
+                .isBetween(-10.00, -1.0);
+
     }
 
 }

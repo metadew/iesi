3462ad279611431c40617dddbd1e627e2ae1450d
Brice Bartoletti <bartoletti.brice@accenture.com>
2020-06-17 22:05:22 +0200

    Convert data return to client to Dto

--- core/java/iesi-rest-without-microservices/src/main/java/io/metadew/iesi/server/rest/script/result/ScriptResultController.java
+++ core/java/iesi-rest-without-microservices/src/main/java/io/metadew/iesi/server/rest/script/result/ScriptResultController.java
@@ -1,6 +1,6 @@
 package io.metadew.iesi.server.rest.script.result;
 
-import io.metadew.iesi.metadata.definition.script.result.ScriptResult;
+import io.metadew.iesi.server.rest.script.result.dto.ScriptResultDto;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
 @Tag(name = "scriptResults", description = "Everything about scriptResults")
@@ -23,18 +23,27 @@ public class ScriptResultController {
     }
 
     @GetMapping
-    public List<ScriptResult> getAll() {
-        return scriptResultService.getAll();
+    public List<ScriptResultDto> getAll() {
+        return scriptResultService.getAll().stream()
+                .map(ScriptResultDto::new)
+                .collect(Collectors.toList());
     }
 
     @GetMapping("/{runId}")
-    public Optional<List<ScriptResult>> getByRunId(@PathVariable String runId) {
-        return scriptResultService.getByRunId(runId);
+    public List<ScriptResultDto> getByRunId(@PathVariable String runId) {
+        return scriptResultService.getByRunId(runId)
+                .map(results -> results.stream()
+                        .map(ScriptResultDto::new)
+                        .collect(Collectors.toList())
+                )
+                .orElse(null);
     }
 
     @GetMapping("/{runId}/{processId}")
-    public Optional<ScriptResult> getByRunIdAndProcessId(@PathVariable String runId, @PathVariable Long processId) {
-        return scriptResultService.getByRunIdAndProcessId(runId, processId);
+    public ScriptResultDto getByRunIdAndProcessId(@PathVariable String runId, @PathVariable Long processId) {
+        return scriptResultService.getByRunIdAndProcessId(runId, processId)
+                .map(ScriptResultDto::new)
+                .orElse(null);
     }
 
 }
---
+++ core/java/iesi-rest-without-microservices/src/main/java/io/metadew/iesi/server/rest/script/result/dto/ScriptResultDto.java
@@ -0,0 +1,44 @@
+package io.metadew.iesi.server.rest.script.result.dto;
+
+import io.metadew.iesi.common.configuration.ScriptRunStatus;
+import io.metadew.iesi.metadata.definition.script.result.ScriptResult;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import org.springframework.hateoas.RepresentationModel;
+
+import java.time.LocalDateTime;
+
+@Data
+@EqualsAndHashCode(callSuper = false)
+@AllArgsConstructor
+@NoArgsConstructor
+public class ScriptResultDto extends RepresentationModel<ScriptResultDto> {
+
+    // ScriptResultKey
+    private String runID;
+    private Long processId;
+
+    private Long parentProcessId;
+    private String scriptId;
+    private String scriptName;
+    private Long scriptVersion;
+    private String environment;
+    private ScriptRunStatus status; // Enum
+    private LocalDateTime startTimestamp;
+    private LocalDateTime endTimestamp;
+
+    public ScriptResultDto(ScriptResult scriptResult){
+        this.runID = scriptResult.getMetadataKey().getRunId();
+        this.processId = scriptResult.getMetadataKey().getProcessId();
+        this.parentProcessId = scriptResult.getParentProcessId();
+        this.scriptId = scriptResult.getScriptId();
+        this.scriptName = scriptResult.getScriptName();
+        this.scriptVersion = scriptResult.getScriptVersion();
+        this.environment = scriptResult.getEnvironment();
+        this.status = scriptResult.getStatus();
+        this.startTimestamp = scriptResult.getStartTimestamp();
+        this.endTimestamp = scriptResult.getEndTimestamp();
+    }
+}

4fe1218f7e4af68fef0e1814b64a4447962d6829
Brice Bartoletti <bartoletti.brice@accenture.com>
2020-06-22 15:54:57 +0200

    getRunById Tests

--- core/java/iesi-rest-without-microservices/src/test/java/io/metadew/iesi/server/rest/script/result/ScriptResultServiceTest.java
+++ core/java/iesi-rest-without-microservices/src/test/java/io/metadew/iesi/server/rest/script/result/ScriptResultServiceTest.java
@@ -19,6 +19,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -43,6 +44,13 @@ public class ScriptResultServiceTest {
     }
 
     @Test
+    void getAllTypeReturnedTest() {
+        assertThat(scriptResultService.getAll())
+                .as("The returned type should an implementation of List")
+                .isInstanceOf(List.class);
+    }
+
+    @Test
     void getAllNoScriptResultTest() {
         assertThat(scriptResultService.getAll().size())
                 .as("No ScriptResult should be found")
@@ -51,7 +59,7 @@ public class ScriptResultServiceTest {
 
     @Test
     void getAllConformity1ScriptResultTest() {
-        ScriptResult insertedScriptResult = createAndInsertADummyScriptResultN(1, "1");
+        ScriptResult insertedScriptResult = createAndInsertADummyScriptResultN(1);
         List<ScriptResult> scriptResultList = scriptResultService.getAll();
 
         assertThat(scriptResultList.size())
@@ -65,8 +73,8 @@ public class ScriptResultServiceTest {
 
     @Test
     void getAllConformityBetweenInsertedAndRetrieved2ScriptResultTest() {
-        ScriptResult insertedScriptResult = createAndInsertADummyScriptResultN(1, "1");
-        ScriptResult insertedScriptResult2 = createAndInsertADummyScriptResultN(2, "2");
+        ScriptResult insertedScriptResult = createAndInsertADummyScriptResultN(1);
+        ScriptResult insertedScriptResult2 = createAndInsertADummyScriptResultN(2);
         List<ScriptResult> scriptResultList = scriptResultService.getAll();
 
         assertThat(scriptResultList.size())
@@ -81,9 +89,9 @@ public class ScriptResultServiceTest {
 
     @Test
     void getAllConformityBetweenInsertedAndRetrieved3ScriptResultTest() {
-        ScriptResult insertedScriptResult = createAndInsertADummyScriptResultN(1, "1");
-        ScriptResult insertedScriptResult2 = createAndInsertADummyScriptResultN(2, "2");
-        ScriptResult insertedScriptResult3 = createAndInsertADummyScriptResultN(3, "3");
+        ScriptResult insertedScriptResult = createAndInsertADummyScriptResultN(1);
+        ScriptResult insertedScriptResult2 = createAndInsertADummyScriptResultN(2);
+        ScriptResult insertedScriptResult3 = createAndInsertADummyScriptResultN(3);
         List<ScriptResult> scriptResultList = scriptResultService.getAll();
 
         assertThat(scriptResultList.size())
@@ -101,25 +109,37 @@ public class ScriptResultServiceTest {
     void getAllConformityBetweenInsertedAndRetrieved20ScriptResultTest() {
         ScriptResult[] arrayOfScriptResult = new ScriptResult[20];
         for (int i = 0; i < arrayOfScriptResult.length; i++)
-            arrayOfScriptResult[i] = createAndInsertADummyScriptResultN(i, String.format("%s", i));
+            arrayOfScriptResult[i] = createAndInsertADummyScriptResultN(i);
 
         List<ScriptResult> scriptResultList = scriptResultService.getAll();
+
+        assertThat(scriptResultList.size())
+                .as("There should be 20 ScriptResult")
+                .isEqualTo(20);
+
         for (ScriptResult currentScriptResult : arrayOfScriptResult)
             assertThat(scriptResultList)
-                    .as("All the inserted ScriptResults should be retrieved")
+                    .as("All the inserted ScriptResults should be retrieved in this list")
                     .contains(currentScriptResult);
     }
 
     @Test
     void getAll1NegativeTestCase() {
         for (int i = 0; i < 20; i++)
-            createAndInsertADummyScriptResultN(i,String.format("%s", i));
+            createAndInsertADummyScriptResultN(i);
 
-        ScriptResult notInsertedScriptResult = createADummyScriptResult(50,"50");
+        ScriptResult notInsertedScriptResult = createADummyScriptResult(50);
         List<ScriptResult> scriptResultList = scriptResultService.getAll();
         assertThat(scriptResultList)
                 .as("All the inserted ScriptResults should be retrieved")
                 .doesNotContain(notInsertedScriptResult);
+    }
+
+    @Test
+    void getByRunIdTypeReturnedTest() {
+        assertThat(scriptResultService.getByRunId("1"))
+                .as("The returned type should an implementation of List")
+                .isInstanceOf(List.class);
     }
 
     @Test
@@ -131,27 +151,158 @@ public class ScriptResultServiceTest {
 
     @Test
     void getByRunIdNoScriptResult20SRTest() {
+        for (int i = 0; i < 20; i++)
+            createAndInsertADummyScriptResultN(i);
 
+        List<ScriptResult> scriptResultList = scriptResultService.getByRunId("notContainedId");
+        assertThat(scriptResultList.size())
+                .as("There shouldn't be any result in this list")
+                .isEqualTo(0);
     }
 
     @Test
-    void getByRunId1ScriptResult20SRTest() {
+    void getByRunId1ScriptResult21SRTest() {
+        for (int i = 0; i < 20; i++)
+            createAndInsertADummyScriptResultN(i);
 
+        ScriptResult containedScriptResult = createAndInsertADummyScriptResultN(20, "containedId");
+
+        List<ScriptResult> scriptResultList = scriptResultService.getByRunId("containedId");
+
+        assertThat(scriptResultList.size())
+                .as("There should be one result in this list")
+                .isEqualTo(1);
+
+        assertThat(scriptResultList)
+                .as("This list should contain this exact result")
+                .contains(containedScriptResult);
     }
 
     @Test
-    void getByRunId2ScriptResult20SRTest() {
+    void getByRunId2ScriptResult22SRTest() {
+        for (int i = 0; i < 20; i++)
+            createAndInsertADummyScriptResultN(i);
 
+        ScriptResult containedScriptResult1 = createAndInsertADummyScriptResultN(20, "containedId");
+        ScriptResult containedScriptResult2 = createAndInsertADummyScriptResultN(21, "containedId");
+        List<ScriptResult> scriptResultList = scriptResultService.getByRunId("containedId");
+
+        assertThat(scriptResultList.size())
+                .as("There should be one result in this list")
+                .isEqualTo(2);
+
+        assertThat(scriptResultList)
+                .as("This list should contain those exact results")
+                .contains(containedScriptResult1)
+                .contains(containedScriptResult2);
     }
 
     @Test
-    void getByRunId3ScriptResult20SRTest() {
+    void getByRunId3ScriptResult23SRTest() {
+        for (int i = 0; i < 20; i++)
+            createAndInsertADummyScriptResultN(i);
 
+        ScriptResult containedScriptResult1 = createAndInsertADummyScriptResultN(20, "containedId");
+        ScriptResult containedScriptResult2 = createAndInsertADummyScriptResultN(21, "containedId");
+        ScriptResult containedScriptResult3 = createAndInsertADummyScriptResultN(22, "containedId");
+        List<ScriptResult> scriptResultList = scriptResultService.getByRunId("containedId");
+
+        assertThat(scriptResultList.size())
+                .as("There should be one result in this list")
+                .isEqualTo(3);
+
+        assertThat(scriptResultList)
+                .as("This list should contain those exact results")
+                .contains(containedScriptResult1)
+                .contains(containedScriptResult2)
+                .contains(containedScriptResult3);
     }
+
+    @Test
+    void getByRunId20ScriptResult20SRTest() {
+        ScriptResult[] arrayOfScriptResult = new ScriptResult[20];
+        for (int i = 0; i < arrayOfScriptResult.length; i++)
+            arrayOfScriptResult[i] = createAndInsertADummyScriptResultN(i, "containedId");
+
+        List<ScriptResult> scriptResultList = scriptResultService.getByRunId("containedId");
+
+        assertThat(scriptResultList.size())
+                .as("There should be 20 retrieved ScriptResult on 20")
+                .isEqualTo(20);
+
+        for (ScriptResult currentScriptResult : arrayOfScriptResult)
+            assertThat(scriptResultList)
+                    .as("All the inserted ScriptResults should be retrieved")
+                    .contains(currentScriptResult);
+    }
+
+    @Test
+    void getByRunId2x20ScriptResult60SRTest() {
+        ScriptResult[][] scriptResults2D = new ScriptResult[3][20];
+
+        for (int i = 0; i < scriptResults2D.length; i++) {
+            for (int j = 0; j < scriptResults2D[i].length; j++) {
+                int unique = Integer.parseInt(String.format("%s%s", i, j));
+                scriptResults2D[i][j] = createAndInsertADummyScriptResultN(unique,
+                        i == 0 ? "containedId1" :
+                                i == 1 ? "containedId2" :
+                                        String.format("%s", j)
+                );
+            }
+        }
+
+        List<ScriptResult> scriptResultId1List = scriptResultService.getByRunId("containedId1");
+        List<ScriptResult> scriptResultId2List = scriptResultService.getByRunId("containedId2");
+
+        assertThat(scriptResultId1List.size())
+                .as("")
+                .isEqualTo(20);
+
+        assertThat(scriptResultId2List.size())
+                .as("")
+                .isEqualTo(20);
+
+        for (int i = 0; i < scriptResults2D.length; i++) {
+            for (int j = 0; j < scriptResults2D[i].length; j++) {
+                if (i == 0) {
+                    assertThat(scriptResultId1List)
+                            .as(String.format("The scriptResult[%s][%s] should be contained in this list (scriptResultId1List)", i, j))
+                            .contains(scriptResults2D[i][j]);
+                } else if (i == 1) {
+                    assertThat(scriptResultId2List)
+                            .as(String.format("The scriptResult[%s][%s] should be contained in this list (scriptResultId2List)", i, j))
+                            .contains(scriptResults2D[i][j]);
+                } else {
+                    assertThat(scriptResultId1List)
+                            .as(String.format("The scriptResult[%s][%s] shouldn't be contained in this list (scriptResultId1List)", i, j))
+                            .doesNotContain(scriptResults2D[i][j]);
+                    assertThat(scriptResultId2List)
+                            .as(String.format("The scriptResult[%s][%s] shouldn't be contained in this list (scriptResultId2List)", i, j))
+                            .doesNotContain(scriptResults2D[i][j]);
+                }
+            }
+        }
+    }
+
 
     @Test
     void getByRunId1NegativeTestCase() {
+        ScriptResult[] arrayOfScriptResult = new ScriptResult[20];
+        for (int i = 0; i < arrayOfScriptResult.length; i++)
+            arrayOfScriptResult[i] = createAndInsertADummyScriptResultN(i, "containedId");
 
+        List<ScriptResult> scriptResultList = scriptResultService.getByRunId("notContainedId");
+
+        assertThat(scriptResultList.size())
+                .as("There should be 20 retrieved ScriptResult on 20")
+                .isEqualTo(0);
+    }
+
+    @Test
+    void getByRunIdAndProcessIdTypeReturnedTest() {
+        assertThat(scriptResultService.getByRunIdAndProcessId("", 1L))
+                .as("The returned type should be an optional")
+                .isInstanceOf(Optional.class);
     }
 
     @Test
@@ -175,6 +326,10 @@ public class ScriptResultServiceTest {
         return scriptResultN;
     }
 
+    ScriptResult createAndInsertADummyScriptResultN(int n) {
+        return createAndInsertADummyScriptResultN(n, String.format("%s", n));
+    }
+
     ScriptResult createADummyScriptResult(int n, String runId) {
         String nString = String.valueOf(n);
         LocalDateTime now = LocalDateTime.now();
@@ -184,15 +339,19 @@ public class ScriptResultServiceTest {
         int nMin = now.getMinute();
 
         return ScriptResult.builder()
-                .scriptResultKey(new ScriptResultKey(nString, (long) n))
+                .scriptResultKey(new ScriptResultKey(runId, (long) n))
                 .parentProcessId((long) n)
                 .scriptId(nString)
-                .scriptName("Script" + runId)
+                .scriptName("Script" + nString)
                 .scriptVersion((long) n)
                 .environment(nString)
                 .status(ScriptRunStatus.SUCCESS)
                 .startTimestamp(LocalDateTime.of(2000, nMonth, nDay, nHours, nMin))
                 .endTimestamp(LocalDateTime.of(2000, nMonth, nDay, nHours, nMin))
                 .build();
+    }
+
+    ScriptResult createADummyScriptResult(int n) {
+        return createADummyScriptResult(n, String.format("%s", n));
     }
 }

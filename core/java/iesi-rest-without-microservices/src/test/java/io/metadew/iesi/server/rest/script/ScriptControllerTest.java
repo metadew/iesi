a1d87b02a9a077ab78fb508aa1f6b7f02e74f195
Brice Bartoletti <bartoletti.brice@accenture.com>
2020-07-02 15:43:12 +0200

    ScriptControllerTest - init

---
+++ core/java/iesi-rest-without-microservices/src/test/java/io/metadew/iesi/server/rest/script/ScriptControllerTest.java
@@ -0,0 +1,49 @@
+package io.metadew.iesi.server.rest.script;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+// Todo: configure environnement test
+class ScriptControllerTest {
+
+    @Test
+    void getAll() {
+        // Todo: Write Test
+    }
+
+    @Test
+    void getByName() {
+        // Todo: Write Test
+    }
+
+    @Test
+    void get() {
+        // Todo: Write Test
+    }
+
+    @Test
+    void post() {
+        // Todo: Write Test
+    }
+
+    @Test
+    void putAll() {
+        // Todo: Write Test
+    }
+
+    @Test
+    void put() {
+        // Todo: Write Test
+    }
+
+    @Test
+    void deleteByName() {
+        // Todo: Write Test
+    }
+
+    @Test
+    void delete() {
+        // Todo: Write Test
+    }
+}

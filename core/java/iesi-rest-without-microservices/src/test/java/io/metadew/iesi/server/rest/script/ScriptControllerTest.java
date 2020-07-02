2df5276418e1bf05d01a1c9bb128339b5d7dffb4
Brice Bartoletti <bartoletti.brice@accenture.com>
2020-07-02 18:31:14 +0200

    ScriptControllerTest - Configuration + First test

--- core/java/iesi-rest-without-microservices/src/test/java/io/metadew/iesi/server/rest/script/ScriptControllerTest.java
+++ core/java/iesi-rest-without-microservices/src/test/java/io/metadew/iesi/server/rest/script/ScriptControllerTest.java
@@ -1,14 +1,67 @@
 package io.metadew.iesi.server.rest.script;
 
+import io.metadew.iesi.server.rest.error.CustomGlobalExceptionHandler;
+import io.metadew.iesi.server.rest.script.dto.ScriptDto;
+import io.metadew.iesi.server.rest.script.dto.ScriptDtoModelAssembler;
+import io.metadew.iesi.server.rest.script.dto.ScriptDtoService;
+
+import io.metadew.iesi.server.rest.script.dto.ScriptPostDtoService;
+import io.metadew.iesi.server.rest.script.dto.action.ScriptActionDtoService;
+import io.metadew.iesi.server.rest.script.dto.label.ScriptLabelDtoService;
+import io.metadew.iesi.server.rest.script.dto.parameter.ScriptParameterDtoService;
+import io.metadew.iesi.server.rest.script.dto.version.ScriptVersionDtoService;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+import java.util.List;
 
-// Todo: configure environnement test
+import static org.hamcrest.Matchers.*;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(ScriptController.class)
+@AutoConfigureMockMvc(addFilters = false)
+@ContextConfiguration(classes = {ScriptController.class, CustomGlobalExceptionHandler.class, ScriptDtoModelAssembler.class,
+        ScriptPostDtoService.class, ScriptParameterDtoService.class, ScriptLabelDtoService.class, ScriptActionDtoService.class,
+        ScriptVersionDtoService.class})
 class ScriptControllerTest {
 
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private ScriptDtoService scriptDtoService;
+
+    @MockBean
+    private IScriptService scriptService;
+
     @Test
-    void getAll() {
+    void getAllNoResult() throws Exception {
+        // Todo: Write Test
+        // Mock Service
+        List<ScriptDto> scriptDtoList = new ArrayList<>();
+        given(scriptDtoService.getAll(null,null)).willReturn(scriptDtoList);
+
+        mvc.perform(get("/scripts")
+                .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(status().isOk())
+                .andExpect(jsonPath("$", anEmptyMap()));
+    }
+
+    @Test
+    void getAll2Results() {
         // Todo: Write Test
     }
 
@@ -18,7 +71,7 @@ class ScriptControllerTest {
     }
 
     @Test
-    void get() {
+    void getByNameAndVersion() {
         // Todo: Write Test
     }
 

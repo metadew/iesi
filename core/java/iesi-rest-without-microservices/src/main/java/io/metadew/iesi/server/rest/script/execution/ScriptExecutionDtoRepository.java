784fcc9a44b00a2855b27430c7a1004539e67a43
Brice Bartoletti <bartoletti.brice@accenture.com>
2020-07-10 11:32:40 +0200

    ScriptExecutionDtoRepository - work init

--- core/java/iesi-rest-without-microservices/src/main/java/io/metadew/iesi/server/rest/script/execution/ScriptExecutionDtoRepository.java
+++ core/java/iesi-rest-without-microservices/src/main/java/io/metadew/iesi/server/rest/script/execution/ScriptExecutionDtoRepository.java
@@ -1,22 +1,35 @@
 package io.metadew.iesi.server.rest.script.execution;
 
+import io.metadew.iesi.common.configuration.metadata.repository.MetadataRepositoryConfiguration;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import javax.sql.rowset.CachedRowSet;
+
+//@Log4j2
 @Repository
 public class ScriptExecutionDtoRepository implements IScriptExecutionDtoRepository {
 
-    // Todo: Call the configuration
+    MetadataRepositoryConfiguration metadataRepositoryConfiguration;
+
+    @Autowired
+    ScriptExecutionDtoRepository(MetadataRepositoryConfiguration metadataRepositoryConfiguration){
+        this.metadataRepositoryConfiguration = metadataRepositoryConfiguration;
+    }
 
     @Override
     public ScriptExecutionDto getByRunIdAndProcessId(String runId, String processId) {
+        ScriptExecutionDto scriptExecutionDto;
         // Todo: Create the SQL Query
+        String SQLQuery = "";
 
-        // Todo: Execute the SQL Query
+        CachedRowSet cachedRowSet = metadataRepositoryConfiguration.getResultMetadataRepository()
+                .executeQuery(SQLQuery, "reader");
 
         // Todo: Create a row mapper
 
         // Todo: return the created Object
-
         return null;
     }
 }

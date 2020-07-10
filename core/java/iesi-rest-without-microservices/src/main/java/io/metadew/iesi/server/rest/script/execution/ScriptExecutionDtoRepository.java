a71482ed8c282f92cb96030aa8b69fe1458a7a2b
Brice Bartoletti <bartoletti.brice@accenture.com>
2020-07-10 10:30:19 +0200

    ScriptExecutionDtoRepository - Set up todo list

--- core/java/iesi-rest-without-microservices/src/main/java/io/metadew/iesi/server/rest/script/execution/ScriptExecutionDtoRepository.java
+++ core/java/iesi-rest-without-microservices/src/main/java/io/metadew/iesi/server/rest/script/execution/ScriptExecutionDtoRepository.java
@@ -1,9 +1,22 @@
 package io.metadew.iesi.server.rest.script.execution;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class ScriptExecutionDtoRepository implements IScriptExecutionDtoRepository {
+
+    // Todo: Call the configuration
+
     @Override
     public ScriptExecutionDto getByRunIdAndProcessId(String runId, String processId) {
-        // Todo: Call the DB and create the SQL Query
+        // Todo: Create the SQL Query
+
+        // Todo: Execute the SQL Query
+
+        // Todo: Create a row mapper
+
+        // Todo: return the created Object
+
         return null;
     }
 }
--- core/java/iesi-rest-without-microservices/src/main/java/io/metadew/iesi/server/rest/script/execution/ScriptExecutionDtoService.java
+++ core/java/iesi-rest-without-microservices/src/main/java/io/metadew/iesi/server/rest/script/execution/ScriptExecutionDtoService.java
@@ -1,13 +1,21 @@
 package io.metadew.iesi.server.rest.script.execution;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ScriptExecutionDtoService implements IScriptExecutionDtoService {
 
+    private final IScriptExecutionDtoRepository scriptExecutionDtoRepository;
+
+    @Autowired
+    ScriptExecutionDtoService(IScriptExecutionDtoRepository scriptExecutionDtoRepository){
+        this.scriptExecutionDtoRepository = scriptExecutionDtoRepository;
+    }
+
     @Override
     public ScriptExecutionDto getByRunIdAndProcessId(String runId, String processId) {
-        return null;
+        return scriptExecutionDtoRepository.getByRunIdAndProcessId(runId, processId);
     }
 
 }

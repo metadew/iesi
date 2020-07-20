a5a3078f595781e133f7bb039d3d56ed5cc11ed6
Peter Billen <56635883+peterbillen@users.noreply.github.com>
2020-07-20 11:19:52 +0200

    initial script

---
+++ build/solution/solution-builder.md
@@ -0,0 +1,70 @@
+{% include navigation.html %}
+
+# Build the solution
+
+This page describes how to build a branch from git and assemble the distribution.
+
+## Builder script
+
+### Limitations
+
+The builder script is currently made for debian based linux environments using the bash shell scripting language.
+
+### Accessing the script
+
+The builder script can be found in the `build/scripts` folder.
+
+### Script actions
+
+The builder script automates the following steps that are needed:
+* update the apt package manager
+* install maven if necessary
+* clone the requested branch fron git
+* install any library that is not present in maven central locally
+* build the source code using maven
+  * iesi-core
+  * iesi-rest
+  * iesi-test
+* setup a workspace for deploying the assembly to
+* create the solution assembly
+* create a distribution artefact
+
+### Script arguments
+
+The script can take several arguments:
+
+|argument|description|default|
+|---|---|---|
+|-d=[arg] or -dir=[arg]|The build directory where to run from|current directory|
+|-b=[arg] or -branch=[arg]|The branch to build|develop|
+|-e=[arg] or -exclude=[arg]|The actions to exclude when running the script||
+
+For the exclusion of action, there are several options as listed below:
+
+|exclusion|action|
+|---|---|
+|g|clone the requested branch fron git|
+|m|install any library that is not present in maven central locally|
+|b|build the source code using maven|
+|w|setup a workspace for deploying the assembly to|
+|a|create the solution assembly|
+|d|create a distribution artefact|
+
+Exclusions are added as single characters that are concatenated one after the other. For instance:
+* `-e=g`: excludes the clone from git
+* `-e=gmb`: exclude the clone from git, local maven install and source code build
+
+**Important:**
+* The script can be found in the iesi git repository, however it is not the objective to run the script from inside the folder where it is located.
+* Instead, create another directory outside the structure to run it in.
+* Feel free to copy the script to the build directory
+* Exclude any actions as needed to quickly re-process parts of the script
+
+### Script output
+
+As a result, the script will produce the following inside the build directory:
+* [branch]/[git clone] containing the code that has been built
+* workspace/build/[branch] containing the assembled solution
+* workspace/dist/[branch] containing the compressed archives suffixed with a timestamp
+
+Happy building!!
---
+++ build/solution/solution-builder.sh
@@ -0,0 +1,229 @@
+#!/bin/bash
+
+###################################################
+# Author: Peter Billen
+#
+# Objective: build a branch from git and assemble
+# the distribution
+#
+###################################################
+
+
+echo "*****    Initializing script          *****"
+
+#Debug settings
+fetch_from_git=1
+maven_build_ext=1
+iesi_build_src=1
+iesi_workspace_setup=1
+iesi_assembly=1
+iesi_distribution=1
+exclude=""
+
+#Script settings
+iesi_version=0.4.0
+branch="develop"
+current_dir=$(pwd)
+script_dir=`dirname $0`
+build_dir=${current_dir}
+source_dir=${build_dir}/source
+branch_dir=${source_dir}/${branch}
+repo_dir=${branch_dir}/iesi
+
+
+# Loop through arguments and process them
+for arg in "$@"
+do
+    case $arg in
+        -d=*|--dir=*)
+        build_dir="${arg#*=}"
+        shift
+        ;;
+        -b=*|--branch=*)
+        branch="${arg#*=}"
+        shift
+        ;;
+        -e=*|--exclude=*)
+        exclude="${arg#*=}"
+        shift #g for git, m for maven, b for build, w for workspace, a for assembly,d for distribution
+        ;;
+    esac
+done
+
+#Exclude debug settings if needed
+if [[ $exclude == *"g"* ]]; then
+  fetch_from_git=0
+  echo "Exclude fetch from git"
+fi
+if [[ $exclude == *"m"* ]]; then
+  maven_build_ext=0
+  echo "Exclude maven extension build"
+fi
+if [[ $exclude == *"b"* ]]; then
+  iesi_build_src=0
+  echo "Exclude build from source"
+fi
+if [[ $exclude == *"w"* ]]; then
+  iesi_workspace_setup=0
+  echo "Exclude workspace setup"
+fi
+if [[ $exclude == *"a"* ]]; then
+  iesi_assembly=0
+  echo "Exclude assembly"
+fi
+if [[ $exclude == *"d"* ]]; then
+  iesi_distribution=0
+  echo "Exclude distribution creation"
+fi
+
+echo "*****    Installing Pre-requisites    *****"
+
+echo "*****    Update packages              *****"
+sudo apt-get update
+
+echo "*****    Installing Git               *****"
+PKG_OK=$(dpkg-query -W --showformat='${Status}\n' git 2>/dev/null | grep "install ok installed")
+if [ "" == "$PKG_OK" ]; then
+  sudo apt-get --force-yes --yes install git
+else
+  echo "Skipping, Git is already installed"
+fi
+
+echo "*****    Installing Rsync             *****"
+PKG_OK=$(dpkg-query -W --showformat='${Status}\n' rsync 2>/dev/null | grep "install ok installed")
+if [ "" == "$PKG_OK" ]; then
+  sudo apt-get --force-yes --yes install rsync
+else
+  echo "Skipping, Rsync is already installed"
+fi
+
+echo "*****    Installing OpenJDK           *****"
+PKG_OK=$(dpkg-query -W --showformat='${Status}\n' openjdk-8-jdk 2>/dev/null | grep "install ok installed")
+if [ "" == "$PKG_OK" ]; then
+  echo "Installing OpenJDK-8"
+  sudo apt-get --force-yes --yes install openjdk-8-jdk
+else
+  echo "Skipping, OpenJDK-8 is already installed"
+fi
+
+echo "*****    Installing Maven             *****"
+PKG_OK=$(dpkg-query -W --showformat='${Status}\n' maven 2>/dev/null | grep "install ok installed")
+if [ "" == "$PKG_OK" ]; then
+  echo "Installing Maven"
+  sudo apt-get --force-yes --yes install maven
+else
+  echo "Skipping, Maven is already installed"
+fi
+
+echo "*****    Installing Pre-requisites    *****"
+
+echo "*****    Fetch branch from GIT        *****"
+
+
+if [ "$fetch_from_git" -eq 1 ]; then
+  
+  mkdir -p ${source_dir}
+  
+  echo "Fetching branch from git"
+  rm -rf ${branch_dir}
+  mkdir -p ${branch_dir}
+  cd ${branch_dir}
+  git clone -b ${branch} --single-branch https://github.com/metadew/iesi.git
+else
+  echo "Skipping, branch fetch from git"
+  cd ${branch_dir}
+fi
+cd iesi
+
+
+echo "*****    Installing build ext         *****"
+if [ "$maven_build_ext" -eq 1 ]; then
+  echo "Installing build ext"
+  cd ${repo_dir}/build/ext
+  while IFS='|' read -r file groupid artifactid version packaging
+  do
+    echo "Installing ${file} into local Maven repository"
+    mvn install:install-file -Dfile=$file -DgroupId=$groupid -DartifactId=$artifactid -Dversion=$version -Dpackaging=$packaging
+  done < "${current_dir}/ext.list"
+  # TODO point to list in repo
+  # XXXXXXXXXXXXXXXXXXXXXXXXXX
+else
+  echo "Skipping, build ext installation"
+fi
+
+
+if [ "$iesi_build_src" -eq 1 ]; then
+  echo
+  echo "*****    Building iesi-core           *****"
+  cd ${repo_dir}/core/java/iesi-core
+  mvn clean install project-info-reports:dependencies -Pdependencies
+
+  echo
+  echo "*****    Building iesi-rest           *****"
+  cd ${repo_dir}/core/java/iesi-rest-without-microservices
+  mvn clean package project-info-reports:dependencies
+
+  echo
+  echo "*****    Building iesi-test           *****"
+  cd ${repo_dir}/core/java/iesi-test
+  mvn clean install project-info-reports:dependencies
+  #-Pdependencies TODO check if needed to add dependencies profile
+else
+  echo "Skipping, iesi source build"
+fi
+
+
+if [ "$iesi_workspace_setup" -eq 1 ]; then
+  echo
+  echo "*****    Setting up workspace         *****"
+  mkdir -p ${build_dir}/workspace
+  mkdir -p ${build_dir}/workspace/conf
+  mkdir -p ${build_dir}/workspace/conf/build
+  mkdir -p ${build_dir}/workspace/build
+  mkdir -p ${build_dir}/workspace/dist
+  
+  workspace_dir=${build_dir}/workspace/conf/build/${branch}
+  rm -rf ${workspace_dir}
+  mkdir -p ${workspace_dir}
+else
+  echo "Skipping, iesi workspace setup"
+fi
+
+jar_file=${repo_dir}/core/java/iesi-core/target/iesi-core-${iesi_version}-jar-with-dependencies.jar
+
+if [ "$iesi_assembly" -eq 1 ]; then
+  echo
+  echo "*****    Running assembly             *****"
+  java -cp ${jar_file} io.metadew.iesi.launch.AssemblyLauncher -repository ${repo_dir} -development ${repo_dir} -sandbox ${build_dir}/workspace -instance build -version ${branch} -configuration ${branch} -distribution
+else
+  echo "Skipping, iesi assembly"
+fi
+
+if [ "$iesi_distribution" -eq 1 ]; then
+  echo
+  echo "*****    Creating distribution        *****"
+  mkdir -p ${build_dir}/workspace/dist/${branch}
+  
+  #Generate uuid
+  uuid=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+  #make it lowercase
+  uuid=${uuid,,}
+  
+  #copy sources
+  rm -rf ${build_dir}/workspace/dist/${branch}/${uuid}
+  mkdir -p ${build_dir}/workspace/dist/${branch}/${uuid}/iesi
+  rsync -ax ${build_dir}/workspace/build/${branch}/. ${build_dir}/workspace/dist/${branch}/${uuid}/iesi/
+  
+  cd ${build_dir}/workspace/dist/${branch}/${uuid}
+  now=$(date +"%Y%m%d%H%M%S%N")
+  tar -zcf ${build_dir}/workspace/dist/${branch}/iesi-dist-${now}.tar.gz .
+  cd ${current_dir}
+  
+  #cleanup
+  rm -rf ${build_dir}/workspace/dist/${branch}/${uuid}
+else
+  echo "Skipping, distribution creation"
+fi
+
+
+echo "*****    Ready Freddy!!                 *****"

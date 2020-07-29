ed8f6f7594dbd897c345c850360d503c37d4142b
Brice Bartoletti <bartoletti.brice@accenture.com>
2020-07-29 12:22:29 +0200

    ScriptExecutionDto - format

--- core/java/iesi-rest-without-microservices/src/main/java/io/metadew/iesi/server/rest/scriptExecutionDto/ScriptExecutionDto.java
+++ core/java/iesi-rest-without-microservices/src/main/java/io/metadew/iesi/server/rest/scriptExecutionDto/ScriptExecutionDto.java
@@ -20,35 +20,22 @@ import java.util.List;
 @NoArgsConstructor
 public class ScriptExecutionDto extends RepresentationModel<ScriptExecutionDto> {
 
-    // runId and processId = PRIMARY KEYS
-    private String runId; // RES_SCRIPT - TRC_DES_SCRIPT - ...
-    private Long processId; // RES_SCRIPT - TRC_DES_SCRIPT - ...
+    // Primary Keys in DB
+    private String runId;
+    private Long processId;
 
-    private Long parentProcessId; // RES_SCRIPT
-    private String scriptId; // RES_SCRIPT
-    private String scriptName; // RES_SCRIPT
-    private Long scriptVersion; // RES_SCRIPT
-    private String environment; // RES_SCRIPT
-    private ScriptRunStatus status; // RES_SCRIPT
-    private LocalDateTime startTimestamp; // RES_SCRIPT
-    private LocalDateTime endTimestamp; // RES_SCRIPT
-
-    // TRC_DES_SCRIPT_PAR - Primary Key: RunID PrcId ScriptParName
-    // InputParameterDto : name, rawValue, ?resolvedValue?
+    private Long parentProcessId;
+    private String scriptId;
+    private String scriptName;
+    private Long scriptVersion;
+    private String environment;
+    private ScriptRunStatus status;
+    private LocalDateTime startTimestamp;
+    private LocalDateTime endTimestamp;
     private List<InputParametersDto> inputParameters = new ArrayList<>();
-
-    // TRC_DES_SCRIPT_LBL - Primary Key: RunId PrcId ScriptLabelId
-    // Label: name, value
     private List<ScriptLabelDto> designLabels = new ArrayList<>();
-
-    // EXE_REQ_LBL OR TRC_SCRIPT_LBL
     private List<ExecutionRequestLabelDto> executionLabels = new ArrayList<>();
-
-    // action: runId, processId, type, name, description, condition,
-    // stopOnError, expectedError, status, startTimestamp, endTimestamp
     private List<ActionExecutionDto> actions = new ArrayList<>();
-
-    // output: name, value
     private List<OutputDto> output = new ArrayList<>();
 
 }

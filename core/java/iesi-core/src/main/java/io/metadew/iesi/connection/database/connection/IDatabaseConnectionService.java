49ad36d639fa746c12a3e7ddf8f88b9a5b2a8405
François <35375877+louis878@users.noreply.github.com>
2020-08-24 10:48:50 +0200

    convert limit specific SQL syntax for Oracle  (#197)

--- core/java/iesi-core/src/main/java/io/metadew/iesi/connection/database/connection/IDatabaseConnectionService.java
+++ core/java/iesi-core/src/main/java/io/metadew/iesi/connection/database/connection/IDatabaseConnectionService.java
@@ -2,7 +2,6 @@ package io.metadew.iesi.connection.database.connection;
 
 import com.zaxxer.hikari.HikariConfig;
 import io.metadew.iesi.connection.database.sql.SqlScriptResult;
-import io.metadew.iesi.data.generation.configuration.Time;
 
 import javax.sql.rowset.CachedRowSet;
 import java.io.IOException;
@@ -49,9 +48,11 @@ public interface IDatabaseConnectionService<T extends DatabaseConnection> {
     public SqlScriptResult executeScript(T databaseConnection, InputStream inputStream, Connection connection) throws IOException, SQLException;
 
     public HikariConfig configure(T databaseConnection, HikariConfig hikariConfig);
+
     @Deprecated
     public PreparedStatement createPreparedStatement(T databaseConnection, Connection connection, String sqlStatement) throws SQLException;
 
     public Class<T> appliesTo();
 
+    public String refactorLimitAndOffset(T databaseConnection, String query);
 }

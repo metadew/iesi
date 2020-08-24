81de834afe45102a743b9b098320a74748b49dc6
Heliodore <francois.heliodore@accenture.com>
2020-08-24 16:39:02 +0200

    add limit for db

--- core/java/iesi-core/src/main/java/io/metadew/iesi/connection/database/presto/PrestoDatabaseConnectionService.java
+++ core/java/iesi-core/src/main/java/io/metadew/iesi/connection/database/presto/PrestoDatabaseConnectionService.java
@@ -4,6 +4,9 @@ import io.metadew.iesi.connection.database.connection.ISchemaDatabaseConnectionS
 import io.metadew.iesi.connection.database.connection.SchemaDatabaseConnectionService;
 import lombok.extern.log4j.Log4j2;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 @Log4j2
 public class PrestoDatabaseConnectionService extends SchemaDatabaseConnectionService<PrestoDatabaseConnection> implements ISchemaDatabaseConnectionService<PrestoDatabaseConnection> {
 
@@ -26,5 +29,22 @@ public class PrestoDatabaseConnectionService extends SchemaDatabaseConnectionSer
     @Override
     public Class<PrestoDatabaseConnection> appliesTo() {
         return PrestoDatabaseConnection.class;
+    }
+
+    public String refactorLimitAndOffset(String query) {
+
+        Pattern pattern = Pattern.compile("(?i)limit\\s+(?<limit>[0-9]*+)\\s+(?i)offset\\s+(?<offset>[0-9]*+)");
+        Matcher matcher = pattern.matcher(query);
+
+        StringBuilder stringBuilder = new StringBuilder(query);
+        while (matcher.find()) {
+            int limit = Integer.parseInt(matcher.group("limit"));
+            int offset = Integer.parseInt(matcher.group("offset"));
+            int matchStartIndex = matcher.start();
+            int matchEndIndex = matcher.end();
+            stringBuilder.replace(matchStartIndex, matchEndIndex, " OFFSET " + offset + " LIMIT " + limit + " ");
+            matcher.reset(stringBuilder);
+        }
+        return stringBuilder.toString();
     }
 }

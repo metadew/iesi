c736935d249b0b88c7c0ef9585441c388a89d763
Heliodore <francois.heliodore@accenture.com>
2020-09-10 16:24:45 +0200

    add

--- core/java/iesi-core/src/main/java/io/metadew/iesi/metadata/configuration/Configuration.java
+++ core/java/iesi-core/src/main/java/io/metadew/iesi/metadata/configuration/Configuration.java
@@ -22,7 +22,7 @@ public abstract class Configuration<T extends Metadata, V extends MetadataKey> {
     // TODO: make singleton
     public Configuration() {
     }
-
+//hello
     public abstract Optional<T> get(V metadataKey);
 
     public abstract List<T> getAll() throws SQLException;

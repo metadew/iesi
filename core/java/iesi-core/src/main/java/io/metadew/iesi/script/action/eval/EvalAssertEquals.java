7ea4169c6a874cade1aca07faace4df25b29977c
Robbe Berrevoets <6872974+BerrevoetsRobbe@users.noreply.github.com>
2020-10-09 10:50:23 +0200

    comparison of templates of datasets

--- core/java/iesi-core/src/main/java/io/metadew/iesi/script/action/eval/EvalAssertEquals.java
+++ core/java/iesi-core/src/main/java/io/metadew/iesi/script/action/eval/EvalAssertEquals.java
@@ -1,7 +1,11 @@
 package io.metadew.iesi.script.action.eval;
 
+import io.metadew.iesi.datatypes.DataType;
 import io.metadew.iesi.datatypes.DataTypeHandler;
+import io.metadew.iesi.datatypes.dataset.Dataset;
+import io.metadew.iesi.datatypes.template.TemplateService;
 import io.metadew.iesi.metadata.definition.action.ActionParameter;
+import io.metadew.iesi.metadata.definition.template.Template;
 import io.metadew.iesi.script.action.ActionTypeExecution;
 import io.metadew.iesi.script.execution.ActionExecution;
 import io.metadew.iesi.script.execution.ExecutionControl;
@@ -10,9 +14,12 @@ import io.metadew.iesi.script.operation.ActionParameterOperation;
 
 public class EvalAssertEquals extends ActionTypeExecution {
 
+    private static final String EXPECTED_KEY = "expected";
+    private static final String ACTUAL_KEY = "actual";
+
     // Parameters
-    private ActionParameterOperation expectedValue;
-    private ActionParameterOperation actualValue;
+    private DataType expectedValue;
+    private DataType actualValue;
 
     public EvalAssertEquals(ExecutionControl executionControl,
                             ScriptExecution scriptExecution, ActionExecution actionExecution) {
@@ -21,31 +28,41 @@ public class EvalAssertEquals extends ActionTypeExecution {
 
     public void prepare() {
         // Reset Parameters
-        this.expectedValue = new ActionParameterOperation(getExecutionControl(),
-                getActionExecution(), getActionExecution().getAction().getType(), "expected");
-        this.actualValue = new ActionParameterOperation(getExecutionControl(),
-                getActionExecution(), getActionExecution().getAction().getType(), "actual");
+        ActionParameterOperation expectedValueActionParameterOperation = new ActionParameterOperation(getExecutionControl(),
+                getActionExecution(), getActionExecution().getAction().getType(), EXPECTED_KEY);
+        ActionParameterOperation actualValueActionParameterOperation = new ActionParameterOperation(getExecutionControl(),
+                getActionExecution(), getActionExecution().getAction().getType(), ACTUAL_KEY);
 
         // Get Parameters
         for (ActionParameter actionParameter : getActionExecution().getAction().getParameters()) {
-            if (actionParameter.getMetadataKey().getParameterName().equalsIgnoreCase("expected")) {
-                expectedValue.setInputValue(actionParameter.getValue(), getExecutionControl().getExecutionRuntime());
-            } else if (actionParameter.getMetadataKey().getParameterName().equalsIgnoreCase("actual")) {
-                actualValue.setInputValue(actionParameter.getValue(), getExecutionControl().getExecutionRuntime());
+            if (actionParameter.getMetadataKey().getParameterName().equalsIgnoreCase(EXPECTED_KEY)) {
+                expectedValueActionParameterOperation.setInputValue(actionParameter.getValue(), getExecutionControl().getExecutionRuntime());
+            } else if (actionParameter.getMetadataKey().getParameterName().equalsIgnoreCase(ACTUAL_KEY)) {
+                actualValueActionParameterOperation.setInputValue(actionParameter.getValue(), getExecutionControl().getExecutionRuntime());
             }
         }
 
         // Create parameter list
-        getActionParameterOperationMap().put("expected", expectedValue);
-        getActionParameterOperationMap().put("actual", actualValue);
+        getActionParameterOperationMap().put(EXPECTED_KEY, expectedValueActionParameterOperation);
+        getActionParameterOperationMap().put(ACTUAL_KEY, actualValueActionParameterOperation);
+
+        expectedValue = expectedValueActionParameterOperation.getValue();
+        actualValue = actualValueActionParameterOperation.getValue();
+
     }
 
     protected boolean executeAction() throws InterruptedException {
-        boolean evaluation = DataTypeHandler.getInstance().equals(expectedValue.getValue(), actualValue.getValue(), getExecutionControl().getExecutionRuntime());
+        boolean evaluation;
+        if (expectedValue instanceof Template && actualValue instanceof Dataset) {
+            evaluation = TemplateService.getInstance().matches(actualValue, (Template) expectedValue, getExecutionControl().getExecutionRuntime());
+        } else {
+            evaluation = DataTypeHandler.getInstance().equals(expectedValue, actualValue, getExecutionControl().getExecutionRuntime());
+        }
+
         if (evaluation) {
             getActionExecution().getActionControl().increaseSuccessCount();
         } else {
-            getActionExecution().getActionControl().logOutput("action.error", "Actual value '" + actualValue.getValue() + "' does not match '" + expectedValue.getValue() + "'");
+            getActionExecution().getActionControl().logOutput("action.error", "Actual value '" + actualValue + "' does not match '" + expectedValue + "'");
             getActionExecution().getActionControl().increaseErrorCount();
         }
         return evaluation;

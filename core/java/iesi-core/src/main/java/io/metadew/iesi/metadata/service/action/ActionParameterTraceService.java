dc31c0c647dbc27cbd315bbb1479375f4b41d6b5
Robbe Berrevoets <6872974+BerrevoetsRobbe@users.noreply.github.com>
2020-10-09 12:36:27 +0200

    trace action parameter as raw string instead of interpreted strings

--- core/java/iesi-core/src/main/java/io/metadew/iesi/metadata/service/action/ActionParameterTraceService.java
+++ core/java/iesi-core/src/main/java/io/metadew/iesi/metadata/service/action/ActionParameterTraceService.java
@@ -3,11 +3,11 @@ package io.metadew.iesi.metadata.service.action;
 import io.metadew.iesi.datatypes.DataType;
 import io.metadew.iesi.datatypes.array.Array;
 import io.metadew.iesi.datatypes.dataset.Dataset;
-import io.metadew.iesi.datatypes.dataset.DatasetHandler;
 import io.metadew.iesi.datatypes.text.Text;
 import io.metadew.iesi.metadata.configuration.action.trace.ActionParameterTraceConfiguration;
 import io.metadew.iesi.metadata.definition.action.trace.ActionParameterTrace;
 import io.metadew.iesi.metadata.definition.action.trace.key.ActionParameterTraceKey;
+import io.metadew.iesi.metadata.definition.template.Template;
 import io.metadew.iesi.script.execution.ActionExecution;
 import lombok.extern.log4j.Log4j2;
 import org.apache.logging.log4j.LogManager;
@@ -64,15 +64,19 @@ public class ActionParameterTraceService {
         } else if (value instanceof Text) {
             actionParameterTraces.add(new ActionParameterTrace(new ActionParameterTraceKey(actionExecution.getExecutionControl().getRunId(), actionExecution.getProcessId(), actionExecution.getAction().getMetadataKey().getActionId(), key), ((Text) value).getString()));
         } else if (value instanceof Array) {
-            int counter = 0;
-            for (DataType element : ((Array) value).getList()) {
-                actionParameterTraces.addAll(getActionParameterTraces(actionExecution, key + counter, element));
-                counter++;
-            }
+            actionParameterTraces.add(new ActionParameterTrace(new ActionParameterTraceKey(actionExecution.getExecutionControl().getRunId(), actionExecution.getProcessId(), actionExecution.getAction().getMetadataKey().getActionId(), key), value.toString()));
+//            int counter = 0;
+//            for (DataType element : ((Array) value).getList()) {
+//                actionParameterTraces.addAll(getActionParameterTraces(actionExecution, key + counter, element));
+//                counter++;
+//            }
         } else if (value instanceof Dataset) {
-            for (Map.Entry<String, DataType> datasetItem : DatasetHandler.getInstance().getDataItems((Dataset) value, actionExecution.getExecutionControl().getExecutionRuntime()).entrySet()) {
-                actionParameterTraces.addAll(getActionParameterTraces(actionExecution, key + datasetItem.getKey(), datasetItem.getValue()));
-            }
+            actionParameterTraces.add(new ActionParameterTrace(new ActionParameterTraceKey(actionExecution.getExecutionControl().getRunId(), actionExecution.getProcessId(), actionExecution.getAction().getMetadataKey().getActionId(), key), value.toString()));
+//            for (Map.Entry<String, DataType> datasetItem : DatasetHandler.getInstance().getDataItems((Dataset) value, actionExecution.getExecutionControl().getExecutionRuntime()).entrySet()) {
+//                actionParameterTraces.addAll(getActionParameterTraces(actionExecution, key + datasetItem.getKey(), datasetItem.getValue()));
+//            }
+        } else if (value instanceof Template) {
+            actionParameterTraces.add(new ActionParameterTrace(new ActionParameterTraceKey(actionExecution.getExecutionControl().getRunId(), actionExecution.getProcessId(), actionExecution.getAction().getMetadataKey().getActionId(), key), value.toString()));
         } else {
             LOGGER.warn(MessageFormat.format("DataType ''{0}'' is unknown to trace", value.getClass()));
         }

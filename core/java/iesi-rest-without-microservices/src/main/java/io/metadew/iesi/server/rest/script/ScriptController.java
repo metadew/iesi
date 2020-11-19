d29d834e0433fe5c7e30337fda02eba9d96c8682
Ouamar <tawbane.ouamar@accenture.com>
2020-11-19 15:26:31 +0100

    last update

--- core/java/iesi-rest-without-microservices/src/main/java/io/metadew/iesi/server/rest/script/ScriptController.java
+++ core/java/iesi-rest-without-microservices/src/main/java/io/metadew/iesi/server/rest/script/ScriptController.java
@@ -9,7 +9,6 @@ import io.metadew.iesi.metadata.tools.IdentifierTools;
 import io.metadew.iesi.server.rest.error.DataBadRequestException;
 import io.metadew.iesi.server.rest.resource.HalMultipleEmbeddedResource;
 import io.metadew.iesi.server.rest.script.dto.*;
-import io.metadew.iesi.server.rest.script.dto.version.ScriptVersionDto;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ByteArrayResource;
@@ -124,7 +123,6 @@ public class ScriptController {
                 .build();
         HttpHeaders httpHeaders = new HttpHeaders();
         httpHeaders.setContentDisposition(contentDisposition);
-
 
         String jsonString = objectMapper.writeValueAsString(scriptDto);
 

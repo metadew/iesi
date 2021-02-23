f4fcdf6122a303eba956ae9656e3e63978b10145
Robbe Berrevoets <6872974+BerrevoetsRobbe@users.noreply.github.com>
2021-02-23 19:11:26 +0100

    add tests

--- core/java/iesi-rest-without-microservices/src/test/java/io/metadew/iesi/server/rest/configuration/security/IesiUserDetailsManagerTest.java
+++ core/java/iesi-rest-without-microservices/src/test/java/io/metadew/iesi/server/rest/configuration/security/IesiUserDetailsManagerTest.java
@@ -1,118 +1,86 @@
 package io.metadew.iesi.server.rest.configuration.security;
 
-import io.metadew.iesi.metadata.definition.user.User;
+import io.metadew.iesi.metadata.definition.security.SecurityGroup;
+import io.metadew.iesi.metadata.definition.security.SecurityGroupKey;
+import io.metadew.iesi.metadata.definition.user.Team;
+import io.metadew.iesi.server.rest.Application;
+import io.metadew.iesi.server.rest.configuration.ClockConfiguration;
+import io.metadew.iesi.server.rest.configuration.TestConfiguration;
 import io.metadew.iesi.server.rest.user.IUserService;
+import io.metadew.iesi.server.rest.user.TeamBuilder;
+import io.metadew.iesi.server.rest.user.UserBuilder;
 import io.metadew.iesi.server.rest.user.UserDto;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.context.annotation.DependsOn;
-import org.springframework.security.access.AccessDeniedException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.provisioning.UserDetailsManager;
-import org.springframework.stereotype.Component;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
 
-@Component
-@DependsOn("metadataRepositoryConfiguration")
 @Log4j2
-public class IesiUserDetailsManager implements UserDetailsManager {
+@SpringBootTest(classes = {Application.class, MethodSecurityConfiguration.class, TestConfiguration.class, ClockConfiguration.class},
+        properties = {"spring.main.allow-bean-definition-overriding=true", "iesi.security.enabled=true"})
+@ExtendWith({MockitoExtension.class, SpringExtension.class})
+@ActiveProfiles({"test"})
+@DirtiesContext
+class IesiUserDetailsManagerTest {
 
-    private final IUserService userService;
+    @Autowired
+    private IesiUserDetailsManager iesiUserDetailsManager;
 
-    public IesiUserDetailsManager(IUserService userService) {
-        this.userService = userService;
+    @MockBean
+    private IUserService userService;
+
+    @Test
+    void loadUserByUsernameTest() {
+        Set<SecurityGroup> securityGroups = Stream.of(
+                new SecurityGroup(new SecurityGroupKey(UUID.randomUUID()), "group1", new HashSet<>(), new HashSet<>())
+        ).collect(Collectors.toSet());
+        Map<String, Object> teamInfo = TeamBuilder.generateTeam(1, 1, 2, securityGroups);
+        Team team = (Team) teamInfo.get("team");
+        Map<String, Object> user1Info = UserBuilder.generateUser("user1", team.getRoles(), team.getTeamName(), securityGroups);
+
+        when(userService.get("user1"))
+                .thenReturn(Optional.of((UserDto) user1Info.get("userDto")));
+        iesiUserDetailsManager.loadUserByUsername("user1");
     }
 
-    @Override
-    public IesiUserDetails loadUserByUsername(String name) {
-        User rawUser = userService
-                .getRawUser(name)
-                .orElseThrow(() -> new UsernameNotFoundException("User " + name + " not found"));
+    @Test
+    void getGrantedAuthorities() {
+        Set<SecurityGroup> securityGroups = Stream.of(
+                new SecurityGroup(new SecurityGroupKey(UUID.randomUUID()), "group1", new HashSet<>(), new HashSet<>()),
+                new SecurityGroup(new SecurityGroupKey(UUID.randomUUID()), "group2", new HashSet<>(), new HashSet<>())
+        ).collect(Collectors.toSet());
+        Map<String, Object> teamInfo = TeamBuilder.generateTeam(1, 2, 2, securityGroups);
+        Team team = (Team) teamInfo.get("team");
+        Map<String, Object> user1Info = UserBuilder.generateUser("user1", team.getRoles(), team.getTeamName(), securityGroups);
+        when(userService.get("user1"))
+                .thenReturn(Optional.of((UserDto) user1Info.get("userDto")));
 
-        return new IesiUserDetails(rawUser, getGrantedAuthorities(name));
+        assertThat(iesiUserDetailsManager.getGrantedAuthorities("user1"))
+                .isEqualTo(Stream.of(
+                        new IESIGrantedAuthority("group1", "authority0"),
+                        new IESIGrantedAuthority("group1", "authority1"),
+                        new IESIGrantedAuthority("group2", "authority1"),
+                        new IESIGrantedAuthority("group2", "authority0")
+                ).collect(Collectors.toSet()));
     }
 
-    public Set<IESIGrantedAuthority> getGrantedAuthorities(String username) {
-        UserDto user = userService
-                .get(username)
-                .orElseThrow(() -> new UsernameNotFoundException("User " + username + " not found"));
-        return user.getRoles()
-                .stream()
-                .map(userRoleDto -> userRoleDto.getTeam().getSecurityGroups().stream()
-                        .map(teamSecurityGroupDto -> userRoleDto.getPrivileges().stream()
-                                .map(privilegeDto -> new IESIGrantedAuthority(teamSecurityGroupDto.getName(), privilegeDto.getPrivilege()))
-                                .collect(Collectors.toSet())
-                        )
-                        .flatMap(Set::stream)
-                        .collect(Collectors.toSet()))
-                .flatMap(Set::stream)
-                .collect(Collectors.toSet());
-    }
-
-    @Override
-    public void createUser(UserDetails user) {
-        throw new UnsupportedOperationException("A user cannot be created in this way");
-    }
-
-    @Override
-    public void updateUser(UserDetails user) {
-        throw new UnsupportedOperationException("A user cannot be created in this way");
-    }
-
-    @Override
-    public void deleteUser(String username) {
-        userService.delete(username);
-    }
-
-    @Override
-    public void changePassword(String oldPassword, String newPassword) {
-        Authentication currentUser = SecurityContextHolder.getContext()
-                .getAuthentication();
-
-        if (currentUser == null) {
-            // This would indicate bad coding somewhere
-            throw new AccessDeniedException(
-                    "Can't change password as no Authentication object found in context for current user.");
-        }
-
-        if (oldPassword.equals(newPassword)) {
-            throw new AccessDeniedException("old password cannot be the same as new password");
-
-        }
-
-        String username = currentUser.getName();
-        User user = userService.getRawUser(username)
-                .orElseThrow(() -> new RuntimeException("Could not find user with name " + username));
-        if (!user.getPassword().equals(oldPassword)) {
-            throw new AccessDeniedException("old password does not match existing password");
-        }
-
-        user.setPassword(newPassword);
-        userService.update(user);
-
-        SecurityContextHolder.getContext().setAuthentication(
-                createNewAuthentication(currentUser, newPassword));
-    }
-
-    private Authentication createNewAuthentication(Authentication currentAuth, String newPassword) {
-        UserDetails user = loadUserByUsername(currentAuth.getName());
-
-        UsernamePasswordAuthenticationToken newAuthentication = new UsernamePasswordAuthenticationToken(
-                user, null, user.getAuthorities());
-        newAuthentication.setDetails(currentAuth.getDetails());
-
-        return newAuthentication;
-    }
-
-    @Override
-    public boolean userExists(String username) {
-        return userService.exists(username);
+    @Test
+    void changePasswordTest() {
+        iesiUserDetailsManager.changePassword("test", "test");
     }
 
 }

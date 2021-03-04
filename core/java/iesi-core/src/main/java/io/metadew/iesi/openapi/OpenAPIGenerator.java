7cc974fe403171ddfa1b9253985e08d2496d68e1
Khattabi <hamza.khattabi@accenture.com>
2021-03-04 09:11:01 +0100

    Update the component if it is already existing

--- core/java/iesi-core/src/main/java/io/metadew/iesi/openapi/OpenAPIGenerator.java
+++ core/java/iesi-core/src/main/java/io/metadew/iesi/openapi/OpenAPIGenerator.java
@@ -6,16 +6,19 @@ import com.fasterxml.jackson.databind.ObjectWriter;
 import io.metadew.iesi.common.configuration.metadata.repository.MetadataRepositoryConfiguration;
 import io.metadew.iesi.metadata.configuration.component.ComponentConfiguration;
 import io.metadew.iesi.metadata.configuration.connection.ConnectionConfiguration;
+import io.metadew.iesi.metadata.configuration.exception.MetadataAlreadyExistsException;
 import io.metadew.iesi.metadata.definition.component.Component;
 import io.metadew.iesi.metadata.definition.connection.Connection;
 import io.metadew.iesi.metadata.operation.MetadataRepositoryOperation;
 import io.metadew.iesi.metadata.repository.MetadataRepository;
+import lombok.extern.log4j.Log4j2;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+@Log4j2
 public class OpenAPIGenerator {
     private static OpenAPIGenerator INSTANCE;
 
@@ -37,14 +40,14 @@ public class OpenAPIGenerator {
         try {
             for (Component component : components) {
                 saveComponentInDirectory(writer,target, component, load);
-                if (load) ComponentConfiguration.getInstance().insert(component);
+                if (load) saveComponent(component);
             }
             for (Connection connection : connections) {
                 saveConnectionInDirectory(writer, target,connection, load);
-                if(load) ConnectionConfiguration.getInstance().insert(connection);
+                if(load) saveConnection(connection);
             }
         } catch (IOException e) {
-            e.printStackTrace();
+            log.warn("The target directory doesn't exist, the process is aborted");
         }
     }
 
@@ -60,6 +63,22 @@ public class OpenAPIGenerator {
                 target + File.separator + "Connections.json"
         ), connection);
 
+    }
+
+    private void saveComponent(Component component) {
+        try {
+            ComponentConfiguration.getInstance().insert(component);
+        } catch (MetadataAlreadyExistsException e) {
+            ComponentConfiguration.getInstance().update(component);
+        }
+    }
+
+    private void saveConnection(Connection connection) {
+        try {
+            ConnectionConfiguration.getInstance().insert(connection);
+        } catch (MetadataAlreadyExistsException e) {
+            ConnectionConfiguration.getInstance().update(connection);
+        }
     }
 
 

7239664dc6bcb443f053b47f991d213231594a7b
Khattabi <hamza.khattabi@accenture.com>
2021-03-29 09:30:28 +0200

    concatenate messages in controller advice

--- core/java/iesi-rest-without-microservices/src/main/java/io/metadew/iesi/server/rest/error/CustomGlobalExceptionHandler.java
+++ core/java/iesi-rest-without-microservices/src/main/java/io/metadew/iesi/server/rest/error/CustomGlobalExceptionHandler.java
@@ -19,41 +19,46 @@ import java.util.Map;
 @ControllerAdvice
 public class CustomGlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
-	@ResponseStatus(HttpStatus.NOT_FOUND)
-	@ExceptionHandler(MetadataDoesNotExistException.class)
-	public void handleMetadataDoesNotExistException(HttpServletResponse response) {
-	}
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(MetadataDoesNotExistException.class)
+    public void handleMetadataDoesNotExistException(HttpServletResponse response) {
+    }
 
-	@ResponseStatus(HttpStatus.BAD_REQUEST)
-	@ExceptionHandler(MetadataAlreadyExistsException.class)
-	public void handleMetadataAlreadyExistsException(HttpServletResponse response) {
-	}
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MetadataAlreadyExistsException.class)
+    public void handleMetadataAlreadyExistsException(HttpServletResponse response) {
+    }
 
-	@ExceptionHandler(DataBadRequestException.class)
-	public void HandleBadRequest(HttpServletResponse response) throws IOException {
-		response.sendError(HttpStatus.BAD_REQUEST.value());
-	}
+    @ExceptionHandler(DataBadRequestException.class)
+    public void HandleBadRequest(HttpServletResponse response) throws IOException {
+        response.sendError(HttpStatus.BAD_REQUEST.value());
+    }
 
-	@ResponseStatus(HttpStatus.BAD_REQUEST)
-	@ExceptionHandler(SwaggerParserException.class)
-	@ResponseBody
-	public Map<String, String> handleSwaggerParserException(SwaggerParserException e) {
-		Map<String, String> errMessages = new HashMap<>();
-		errMessages.put("error", "Something is wrong in your documentation");
-		errMessages.put("errorCode", "400");
-		errMessages.put("message", String.format("We found the following errors : %n %s", e.getMessage()));
-		return errMessages;
-	}
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(SwaggerParserException.class)
+    @ResponseBody
+    public Map<String, String> handleSwaggerParserException(SwaggerParserException e) {
+        Map<String, String> errMessages = new HashMap<>();
+        StringBuilder stringBuilder = new StringBuilder();
+        for (String message : e.getMessages()) {
+            stringBuilder.append(String.format("- %s %n", message));
+        }
 
-	@ExceptionHandler(value = { MethodArgumentTypeMismatchException.class })
-	@ResponseStatus(value = HttpStatus.BAD_REQUEST)
-	@ResponseBody
-	public Map<String, String> handleException(MethodArgumentTypeMismatchException e) {
-		Map<String, String> errMessages = new HashMap<>();
-		errMessages.put("error", "Wrong query parameter for '" + e.getName() + "'");
-		errMessages.put("query_parameter", e.getName());
-		errMessages.put("errorCode", e.getErrorCode() + " , " + e.getRootCause().toString());
-		errMessages.put("message", e.getMessage());
-		return errMessages;
-	}
+        errMessages.put("error", "Something is wrong in your documentation");
+        errMessages.put("errorCode", "400");
+        errMessages.put("message", String.format("We found the following errors : %n %s", stringBuilder));
+        return errMessages;
+    }
+
+    @ExceptionHandler(value = {MethodArgumentTypeMismatchException.class})
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    @ResponseBody
+    public Map<String, String> handleException(MethodArgumentTypeMismatchException e) {
+        Map<String, String> errMessages = new HashMap<>();
+        errMessages.put("error", "Wrong query parameter for '" + e.getName() + "'");
+        errMessages.put("query_parameter", e.getName());
+        errMessages.put("errorCode", e.getErrorCode() + " , " + e.getRootCause().toString());
+        errMessages.put("message", e.getMessage());
+        return errMessages;
+    }
 }

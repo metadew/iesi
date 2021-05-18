3e61ff4cdb61a9ff18dba36f6be9f3e8e85225cd
suyash.d.jain <suyash.d.jain@accenture.com>
2021-05-18 15:20:20 +0530

    Added Text Replace Instruction

--- core/java/iesi-core/src/main/java/io/metadew/iesi/script/execution/instruction/data/text/TextReplace.java
+++ core/java/iesi-core/src/main/java/io/metadew/iesi/script/execution/instruction/data/text/TextReplace.java
@@ -1,2 +1,32 @@
-package io.metadew.iesi.script.execution.instruction.data.text;public class TextReplace {
+package io.metadew.iesi.script.execution.instruction.data.text;
+
+import io.metadew.iesi.script.execution.instruction.data.DataInstruction;
+
+import java.text.MessageFormat;
+
+public class TextReplace implements DataInstruction {
+
+    @Override
+    public String generateOutput(String parameters) {
+
+        String [] args = parameters.split(",");
+        if(args.length == 3){
+            String text = args[0];
+            String first = args[1];
+            String end = args[2];
+            text = text.replaceAll(first,end);
+            return text;
+        }else if (args.length==2) {
+            String text = args[0];
+            String first = args[1];
+            text = text.replaceAll(first, "");
+            return text;
+        } else {
+            throw new IllegalArgumentException(MessageFormat.format("Illegal arguments provided to " + this.getKeyword() + ": {0}", parameters));
+        }
+    }
+
+    @Override
+    public String getKeyword() { return "text.replace"; }
+
 }
--- core/java/iesi-core/src/test/java/io/metadew/iesi/script/execution/instruction/data/text/TextReplaceTest.java
+++ core/java/iesi-core/src/test/java/io/metadew/iesi/script/execution/instruction/data/text/TextReplaceTest.java
@@ -1,4 +1,40 @@
 package io.metadew.iesi.script.execution.instruction.data.text;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class TextReplaceTest {
+
+    @Test
+    void textReplace() {
+        TextReplace textReplace = new TextReplace();
+        assertEquals("source dexd", textReplace.generateOutput("source text,t,d"));
+    }
+
+    @Test
+    void textReplaceTwo() {
+        TextReplace textReplace = new TextReplace();
+        assertEquals("source text", textReplace.generateOutput("source text,f,d"));
+    }
+
+    @Test
+    void textReplaceThree() {
+        TextReplace textReplace = new TextReplace();
+        assertEquals("sourcetext", textReplace.generateOutput("source text, ,"));
+    }
+
+    @Test
+    void textReplaceFour() {
+        TextReplace textReplace = new TextReplace();
+        assertEquals("source ex", textReplace.generateOutput("source text,t,"));
+    }
+
+    @Test
+    void textReplaceThrowException() {
+        TextReplace textReplace = new TextReplace();
+        assertThrows(IllegalArgumentException.class, () -> textReplace.generateOutput("source text,,"));
+    }
+
 }

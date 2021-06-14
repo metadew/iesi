692d859d8d453d090d8cfd095efd66acf4e6df80
Robbe Berrevoets <robbeberrevoets@accenture.com>
2021-06-14 16:54:52 +0200

    allow dynamic loading for iesi rest and iesi core

--- core/bin/iesi-rest.sh
+++ core/bin/iesi-rest.sh
@@ -4,6 +4,6 @@ lib_dir=$current_dir/../rest
 
 cd $lib_dir
 
-java -Dlog4j.configurationFile=log4j2.xml -jar iesi-rest.jar
+java -Dlog4j.configurationFile=log4j2.xml -Dloader.path="../plugins" -Dloader.main=io.metadew.iesi.server.rest.Application -cp iesi-rest.jar org.springframework.boot.loader.PropertiesLauncher
 
 cd $current_dir

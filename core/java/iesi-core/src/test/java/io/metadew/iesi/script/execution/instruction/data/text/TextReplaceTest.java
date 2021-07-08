cd07e35678bba204211383d6e845ffd126f0e2f7
suyash.d.jain <suyash.d.jain@accenture.com>
2021-07-08 13:30:26 +0530

    build fix

--- core/java/iesi-core/src/test/java/io/metadew/iesi/script/execution/instruction/data/text/TextReplaceTest.java
+++ core/java/iesi-core/src/test/java/io/metadew/iesi/script/execution/instruction/data/text/TextReplaceTest.java
@@ -10,25 +10,25 @@ class TextReplaceTest {
     @Test
     void textReplace() {
         TextReplace textReplace = new TextReplace();
-        assertEquals("source dexd", textReplace.generateOutput("\"source text\",\"t\",\"d\""));
+        assertEquals("source dexd", textReplace.generateOutput("\"source text\", \"t\", \"d\""));
     }
 
     @Test
     void textReplaceTwo() {
         TextReplace textReplace = new TextReplace();
-        assertEquals("source text", textReplace.generateOutput("\"source text\",\"f\",\"d\""));
+        assertEquals("source text", textReplace.generateOutput("\"source text\", \"f\", \"d\""));
     }
 
     @Test
     void textReplaceThree() {
         TextReplace textReplace = new TextReplace();
-        assertEquals("sourcetext", textReplace.generateOutput("\"source text\",\" \""));
+        assertEquals("sourcetext", textReplace.generateOutput("\"source text\", \" \""));
     }
 
     @Test
     void textReplaceFour() {
         TextReplace textReplace = new TextReplace();
-        assertEquals("source ex", textReplace.generateOutput("\"source text\",\"t\""));
+        assertEquals("source ex", textReplace.generateOutput("\"source text\", \"t\""));
     }
 
     @Test
@@ -40,25 +40,25 @@ class TextReplaceTest {
     @Test
     void textReplaceWithSpecialCharacter() {
         TextReplace textReplace = new TextReplace();
-        assertEquals("01234  05678", textReplace.generateOutput("\"+1234  +5678\",\"+\",\"0\""));
+        assertEquals("01234  05678", textReplace.generateOutput("\"+1234  +5678\", \"+\", \"0\""));
     }
 
     @Test
     void textReplaceByEmptySpace() {
         TextReplace textReplace = new TextReplace();
-        assertEquals("12345678", textReplace.generateOutput("\"+12345678\",\"+\""));
+        assertEquals("12345678", textReplace.generateOutput("\"+12345678\", \"+\""));
     }
 
     @Test
     void textReplaceByEmptySpaceWithTwoArg() {
         TextReplace textReplace = new TextReplace();
-        assertEquals("12345678", textReplace.generateOutput("\"+12+345+678\",\"+\""));
+        assertEquals("12345678", textReplace.generateOutput("\"+12+345+678\", \"+\""));
     }
 
     @Test
     void textReplaceByComma() {
         TextReplace textReplace = new TextReplace();
-        assertEquals("1234.567.8", textReplace.generateOutput("\"1234,567,8\",\",\",\".\""));
+        assertEquals("1234.567.8", textReplace.generateOutput("\"1234,567,8\", \",\", \".\""));
     }
 
 }

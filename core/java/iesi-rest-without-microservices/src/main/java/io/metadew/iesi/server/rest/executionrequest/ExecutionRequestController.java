8dfc2c2f7068ba50a469daa6822202cd0843b809
Suyash Jain <81413098+Suyash-d@users.noreply.github.com>
2021-09-13 14:25:51 +0530

    allow filtering on script execution status for execution requests (#362)

--- core/java/iesi-rest-without-microservices/src/main/java/io/metadew/iesi/server/rest/executionrequest/ExecutionRequestController.java
+++ core/java/iesi-rest-without-microservices/src/main/java/io/metadew/iesi/server/rest/executionrequest/ExecutionRequestController.java
@@ -71,8 +71,9 @@ public class ExecutionRequestController {
                                                   @RequestParam(required = false, name = "version") String version,
                                                   @RequestParam(required = false, name = "environment") String environment,
                                                   @RequestParam(required = false, name = "label") String labelKeyCombination,
-                                                  @RequestParam(required = false, name = "run-id") String runId) {
-        List<ExecutionRequestFilter> executionRequestFilters = extractScriptFilterOptions(script, version, environment, labelKeyCombination, runId);
+                                                  @RequestParam(required = false, name = "run-id") String runId,
+                                                  @RequestParam(required = false, name = "run-status") String runStatus) {
+        List<ExecutionRequestFilter> executionRequestFilters = extractScriptFilterOptions(script, version, environment, labelKeyCombination, runId, runStatus);
         Page<ExecutionRequestDto> executionRequestDtoPage = executionRequestService
                 .getAll(SecurityContextHolder.getContext().getAuthentication(), pageable, executionRequestFilters);
         if (executionRequestDtoPage.hasContent())
@@ -80,7 +81,7 @@ public class ExecutionRequestController {
         return (PagedModel<ExecutionRequestDto>) executionRequestDtoResourceAssemblerPage.toEmptyModel(executionRequestDtoPage, ExecutionRequestDto.class);
     }
 
-    private List<ExecutionRequestFilter> extractScriptFilterOptions(String name, String version, String environment, String labelKeyCombination, String runId) {
+    private List<ExecutionRequestFilter> extractScriptFilterOptions(String name, String version, String environment, String labelKeyCombination, String runId, String runStatus) {
         List<ExecutionRequestFilter> executionRequestFilters = new ArrayList<>();
         if (name != null) {
             executionRequestFilters.add(new ExecutionRequestFilter(ExecutionRequestFilterOption.NAME, name, false));
@@ -96,6 +97,9 @@ public class ExecutionRequestController {
         }
         if (runId != null) {
             executionRequestFilters.add(new ExecutionRequestFilter(ExecutionRequestFilterOption.RUN_ID, runId, false));
+        }
+        if (runStatus != null) {
+            executionRequestFilters.add(new ExecutionRequestFilter(ExecutionRequestFilterOption.STATUS, runStatus, true));
         }
         return executionRequestFilters;
     }

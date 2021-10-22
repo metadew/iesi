2225b672ef7299d9bc83e3330539f12a5369d366
HamzaaKhattabi <h.khattabi@accenture.com>
2021-10-22 16:48:04 +0200

    apply security for connections at UI manipulation level

--- core/java/iesi-core/src/test/java/io/metadew/iesi/openapi/OpenAPIGeneratorTest.java
+++ core/java/iesi-core/src/test/java/io/metadew/iesi/openapi/OpenAPIGeneratorTest.java
@@ -1,5 +1,8 @@
 package io.metadew.iesi.openapi;
 
+import io.metadew.iesi.common.configuration.Configuration;
+import io.metadew.iesi.common.configuration.metadata.repository.MetadataRepositoryConfiguration;
+import io.metadew.iesi.metadata.configuration.security.SecurityGroupConfiguration;
 import io.metadew.iesi.metadata.definition.component.Component;
 import io.metadew.iesi.metadata.definition.component.ComponentParameter;
 import io.metadew.iesi.metadata.definition.component.ComponentVersion;
@@ -11,10 +14,10 @@ import io.metadew.iesi.metadata.definition.connection.ConnectionParameter;
 import io.metadew.iesi.metadata.definition.connection.key.ConnectionKey;
 import io.metadew.iesi.metadata.definition.connection.key.ConnectionParameterKey;
 import io.metadew.iesi.metadata.definition.environment.key.EnvironmentKey;
-import io.metadew.iesi.metadata.definition.security.SecurityGroupKey;
+import io.metadew.iesi.metadata.definition.security.SecurityGroup;
+import io.metadew.iesi.metadata.repository.MetadataRepository;
 import io.metadew.iesi.metadata.tools.IdentifierTools;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -32,6 +35,30 @@ class OpenAPIGeneratorTest {
     byte[] wrongDocFile;
     String title;
     String version;
+
+
+    @BeforeAll
+    static void prepare() {
+        Configuration.getInstance();
+        MetadataRepositoryConfiguration.getInstance()
+                .getMetadataRepositories()
+                .forEach(MetadataRepository::createAllTables);
+    }
+
+    @AfterEach
+    void clearDatabase() {
+        MetadataRepositoryConfiguration.getInstance()
+                .getMetadataRepositories()
+                .forEach(MetadataRepository::cleanAllTables);
+    }
+
+    @AfterAll
+    static void teardown() {
+        Configuration.getInstance();
+        MetadataRepositoryConfiguration.getInstance()
+                .getMetadataRepositories()
+                .forEach(MetadataRepository::dropAllTables);
+    }
 
     @BeforeEach
     public void init() {
@@ -70,6 +97,7 @@ class OpenAPIGeneratorTest {
 
 
     private TransformResult getTransformResult() {
+        SecurityGroup securityGroup = SecurityGroupConfiguration.getInstance().getByName("PUBLIC").orElseThrow(RuntimeException::new);
         EnvironmentKey environmentKey = new EnvironmentKey("env0");
         ConnectionKey connectionKey = new ConnectionKey(
                 "Swagger Petstore - OpenAPI 3.0",
@@ -87,7 +115,7 @@ class OpenAPIGeneratorTest {
         ConnectionParameter tls = new ConnectionParameter(
                 new ConnectionParameterKey(connectionKey, "tls"),
                 "Y");
-        Connection connection = new Connection(connectionKey, new SecurityGroupKey(UUID.randomUUID()), "PUBLIC", "http", "small description", Arrays.asList(baseUrl, host, tls));
+        Connection connection = new Connection(connectionKey, securityGroup.getMetadataKey(), "PUBLIC", "http", "small description", Arrays.asList(baseUrl, host, tls));
 
 
         ComponentParameter endpoint = new ComponentParameter(

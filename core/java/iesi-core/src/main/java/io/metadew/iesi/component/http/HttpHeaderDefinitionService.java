f3800bb2700fac0154e7f3d0fa9290bb5f3cc344
HamzaaKhattabi <h.khattabi@accenture.com>
2021-12-20 12:39:30 +0100

    add missing files that should come from the branch headers definition preferences

--- core/java/iesi-core/src/main/java/io/metadew/iesi/component/http/HttpHeaderDefinitionService.java
+++ core/java/iesi-core/src/main/java/io/metadew/iesi/component/http/HttpHeaderDefinitionService.java
@@ -1,6 +1,13 @@
 package io.metadew.iesi.component.http;
 
 import io.metadew.iesi.metadata.definition.component.ComponentParameter;
+import io.metadew.iesi.script.action.http.KeyValuePairException;
+import io.metadew.iesi.script.action.http.QuoteCharException;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class HttpHeaderDefinitionService implements IHttpHeaderDefinitionService {
 
@@ -17,7 +24,28 @@ public class HttpHeaderDefinitionService implements IHttpHeaderDefinitionService
     }
 
     public HttpHeaderDefinition convert(String httpHeader) {
-        return new HttpHeaderDefinition(httpHeader.split(",", 2)[0], httpHeader.split(",", 2)[1]);
+        List<String> keyValue;
+        String key;
+        String value;
+
+        if (!httpHeader.contains("=")) {
+            throw new KeyValuePairException(String.format("The parameter %s should contain key value pair separated by the equals character < key=\"value\" >.", httpHeader));
+        }
+
+        keyValue = Arrays.stream(httpHeader.split("(?<!\".{0,255}[^\"])=|=(?![^\"].*\")")).collect(Collectors.toList());
+
+        if (keyValue.size() > 2) {
+            throw new KeyValuePairException(String.format("The parameter %s should contain one key value pair, please remove additional separator character.", httpHeader));
+        }
+
+        key = keyValue.get(0);
+        value = keyValue.get(1);
+
+        if (!(value.startsWith("\"") && value.endsWith("\""))) {
+            throw new QuoteCharException(String.format("The value %s is not provided correctly, please use quotes", value));
+        }
+
+        return new HttpHeaderDefinition(key, StringUtils.substringBetween(value, "\"", "\""));
     }
 
     public HttpHeaderDefinition convert(ComponentParameter componentParameter) {

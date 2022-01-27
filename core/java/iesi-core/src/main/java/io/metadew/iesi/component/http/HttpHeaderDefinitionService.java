f1aef211ee0769d359b3486b88de63c73f38bd0f
HamzaaKhattabi <h.khattabi@accenture.com>
2022-01-27 14:48:57 +0100

    update the component headers to use the new implementation

--- core/java/iesi-core/src/main/java/io/metadew/iesi/component/http/HttpHeaderDefinitionService.java
+++ core/java/iesi-core/src/main/java/io/metadew/iesi/component/http/HttpHeaderDefinitionService.java
@@ -1,7 +1,17 @@
 package io.metadew.iesi.component.http;
 
 import io.metadew.iesi.metadata.definition.component.ComponentParameter;
+import io.metadew.iesi.script.action.http.KeyValuePairException;
+import io.metadew.iesi.script.action.http.QuoteCharException;
+import lombok.extern.log4j.Log4j2;
+import org.apache.commons.lang3.StringUtils;
 
+import java.text.MessageFormat;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Log4j2
 public class HttpHeaderDefinitionService implements IHttpHeaderDefinitionService {
 
     private static HttpHeaderDefinitionService INSTANCE;
@@ -17,7 +27,29 @@ public class HttpHeaderDefinitionService implements IHttpHeaderDefinitionService
     }
 
     public HttpHeaderDefinition convert(String httpHeader) {
-        return new HttpHeaderDefinition(httpHeader.split(",", 2)[0], httpHeader.split(",", 2)[1]);
+        log.info(MessageFormat.format("Converting http header {0}", httpHeader));
+        List<String> keyValue;
+        String key;
+        String value;
+
+        if (!httpHeader.contains("=")) {
+            throw new KeyValuePairException(String.format("The parameter %s should contain key value pair separated by the equals character < key=\"value\" >.", httpHeader));
+        }
+
+        keyValue = Arrays.stream(httpHeader.split("(?<!\".{0,255}[^\"])=|=(?![^\"].*\")")).collect(Collectors.toList());
+
+        if (keyValue.size() > 2) {
+            throw new KeyValuePairException(String.format("The parameter %s should contain one key value pair, please remove additional separator character.", httpHeader));
+        }
+
+        key = keyValue.get(0);
+        value = keyValue.get(1);
+
+        if (!(value.startsWith("\"") && value.endsWith("\""))) {
+            throw new QuoteCharException(String.format("The value %s is not provided correctly, please use quotes", value));
+        }
+
+        return new HttpHeaderDefinition(key, StringUtils.substringBetween(value, "\"", "\""));
     }
 
     public HttpHeaderDefinition convert(ComponentParameter componentParameter) {

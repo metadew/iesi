fe4578a031ca70c4a290292c633c9c6ab1f24458
HamzaaKhattabi <91054695+HamzaaKhattabi@users.noreply.github.com>
2022-03-08 10:01:13 +0100

    Adding bugfix for the xml path instructions and spaces preservations. Adding instruction escapements + small adjustments
    * bugfix xml path instruction, spaces preservation, escaping instruction lookup concept
    
    * add instruction escapments
    
    * small ajdustments

--- core/java/iesi-core/src/main/java/io/metadew/iesi/connection/tools/SQLTools.java
+++ core/java/iesi-core/src/main/java/io/metadew/iesi/connection/tools/SQLTools.java
@@ -5,7 +5,6 @@ import io.metadew.iesi.connection.database.DatabaseHandler;
 import io.metadew.iesi.connection.database.connection.DatabaseConnectionHandler;
 
 import javax.sql.rowset.CachedRowSet;
-import javax.sql.rowset.serial.SerialClob;
 import java.io.*;
 import java.sql.*;
 import java.time.LocalDateTime;
@@ -39,13 +38,14 @@ public final class SQLTools {
         StringBuilder sb = new StringBuilder();
         try {
             Reader reader = clob.getCharacterStream();
+            char[] cbuf = new char[2048];
             if (reader == null) {
                 return null;
             }
             BufferedReader br = new BufferedReader(reader);
-            String line;
-            while (null != (line = br.readLine())) {
-                sb.append(line);
+            int line;
+            while (-1 != (line = br.read(cbuf, 0, cbuf.length))) {
+                sb.append(cbuf, 0, line);
             }
             br.close();
         } catch (SQLException | IOException e) {
--- core/java/iesi-core/src/test/java/io/metadew/iesi/script/execution/instruction/data/text/TextReplaceTest.java
+++ core/java/iesi-core/src/test/java/io/metadew/iesi/script/execution/instruction/data/text/TextReplaceTest.java
@@ -1,82 +1,109 @@
 package io.metadew.iesi.script.execution.instruction.data.text;
 
+import io.metadew.iesi.datatypes.DataTypeHandler;
+import io.metadew.iesi.datatypes.text.Text;
+import io.metadew.iesi.script.execution.ExecutionRuntime;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.powermock.reflect.Whitebox;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
 
 class TextReplaceTest {
 
+    DataTypeHandler dataTypeHandler;
+    DataTypeHandler dataTypeHandlerServiceSpy;
+    ExecutionRuntime executionRuntime;
+    TextReplace textReplace;
+
+    @BeforeEach
+    public void before() {
+        dataTypeHandler = DataTypeHandler.getInstance();
+        dataTypeHandlerServiceSpy = Mockito.spy(dataTypeHandler);
+        Whitebox.setInternalState(DataTypeHandler.class, "instance", dataTypeHandlerServiceSpy);
+        executionRuntime = mock(ExecutionRuntime.class);
+        textReplace = new TextReplace(executionRuntime);
+    }
+
+    @AfterEach
+    public void after() {
+        Whitebox.setInternalState(DataTypeHandler.class, "instance", (DataTypeHandler) null);
+    }
+
     @Test
     void textReplace() {
-        TextReplace textReplace = new TextReplace();
+        doReturn(new Text(("\"source text\", \"t\", \"d\""))).when(dataTypeHandlerServiceSpy).resolve("\"source text\", \"t\", \"d\"", executionRuntime);
         assertEquals("source dexd", textReplace.generateOutput("\"source text\", \"t\", \"d\""));
     }
 
     @Test
     void textReplaceTwo() {
-        TextReplace textReplace = new TextReplace();
+        doReturn(new Text(("\"source text\", \"f\", \"d\""))).when(dataTypeHandlerServiceSpy).resolve("\"source text\", \"f\", \"d\"", executionRuntime);
         assertEquals("source text", textReplace.generateOutput("\"source text\", \"f\", \"d\""));
     }
 
     @Test
     void textReplaceThree() {
-        TextReplace textReplace = new TextReplace();
+        doReturn(new Text(("\"source text\", \" \""))).when(dataTypeHandlerServiceSpy).resolve("\"source text\", \" \"", executionRuntime);
         assertEquals("sourcetext", textReplace.generateOutput("\"source text\", \" \""));
     }
 
     @Test
     void textReplaceFour() {
-        TextReplace textReplace = new TextReplace();
+        doReturn(new Text(("\"source text\", \"t\""))).when(dataTypeHandlerServiceSpy).resolve("\"source text\", \"t\"", executionRuntime);
         assertEquals("source ex", textReplace.generateOutput("\"source text\", \"t\""));
     }
 
     @Test
     void textReplaceThrowException() {
-        TextReplace textReplace = new TextReplace();
+        doReturn(new Text(("Some String, For, Illegal, Exception"))).when(dataTypeHandlerServiceSpy).resolve("Some String, For, Illegal, Exception", executionRuntime);
         assertThrows(IllegalArgumentException.class, () -> textReplace.generateOutput("Some String, For, Illegal, Exception"));
     }
 
     @Test
     void textReplaceWithSpecialCharacter() {
-        TextReplace textReplace = new TextReplace();
+        doReturn(new Text(("\"+1234  +5678\", \"+\", \"0\""))).when(dataTypeHandlerServiceSpy).resolve("\"+1234  +5678\", \"+\", \"0\"", executionRuntime);
         assertEquals("01234  05678", textReplace.generateOutput("\"+1234  +5678\", \"+\", \"0\""));
     }
 
     @Test
     void textReplaceByEmptySpace() {
-        TextReplace textReplace = new TextReplace();
+        doReturn(new Text(("\"+12345678\", \"+\""))).when(dataTypeHandlerServiceSpy).resolve("\"+12345678\", \"+\"", executionRuntime);
         assertEquals("12345678", textReplace.generateOutput("\"+12345678\", \"+\""));
     }
 
     @Test
     void textReplaceByEmptySpaceWithTwoArg() {
-        TextReplace textReplace = new TextReplace();
+        doReturn(new Text(("\"+12+345+678\", \"+\""))).when(dataTypeHandlerServiceSpy).resolve("\"+12+345+678\", \"+\"", executionRuntime);
         assertEquals("12345678", textReplace.generateOutput("\"+12+345+678\", \"+\""));
     }
 
     @Test
     void textReplaceByComma() {
-        TextReplace textReplace = new TextReplace();
+        doReturn(new Text(("\"1234,567,8\", \",\", \".\""))).when(dataTypeHandlerServiceSpy).resolve("\"1234,567,8\", \",\", \".\"", executionRuntime);
         assertEquals("1234.567.8", textReplace.generateOutput("\"1234,567,8\", \",\", \".\""));
     }
 
     @Test
     void textReplaceByQuote() {
-        TextReplace textReplace = new TextReplace();
+        doReturn(new Text(("\"sour\"e text\", \"\"\", \"c\""))).when(dataTypeHandlerServiceSpy).resolve("\"sour\"e text\", \"\"\", \"c\"", executionRuntime);
         assertEquals("source text" , textReplace.generateOutput("\"sour\"e text\", \"\"\", \"c\""));
     }
 
     @Test
     void textReplaceInstruction() {
-        TextReplace textReplace = new TextReplace();
+        doReturn(new Text(("     \"source text\"    , \"t\",    \"c\"  "))).when(dataTypeHandlerServiceSpy).resolve("     \"source text\"    , \"t\",    \"c\"  ", executionRuntime);
         assertEquals("source cexc" , textReplace.generateOutput("     \"source text\"    , \"t\",    \"c\"  "));
     }
 
     @Test
     void textReplaceBySpace() {
-        TextReplace textReplace = new TextReplace();
+        doReturn(new Text(("\"         12345678\", \" \""))).when(dataTypeHandlerServiceSpy).resolve("\"         12345678\", \" \"", executionRuntime);
         assertEquals("12345678", textReplace.generateOutput("\"         12345678\", \" \""));
     }
-
 }

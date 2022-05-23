fd5e4f5b45cdb8ff1f83906ba3242730f93f10b8
HamzaaKhattabi <h.khattabi@accenture.com>
2022-05-23 09:19:30 +0200

    update resolved log value on action report detail after getting the latest version of a component in the http.executeRequest action

--- core/java/iesi-core/src/main/java/io/metadew/iesi/script/action/http/HttpExecuteRequest.java
+++ core/java/iesi-core/src/main/java/io/metadew/iesi/script/action/http/HttpExecuteRequest.java
@@ -14,13 +14,10 @@ import io.metadew.iesi.datatypes.dataset.implementation.DatasetImplementation;
 import io.metadew.iesi.datatypes.dataset.implementation.DatasetImplementationHandler;
 import io.metadew.iesi.datatypes.text.Text;
 import io.metadew.iesi.metadata.configuration.action.design.ActionParameterDesignTraceConfiguration;
-import io.metadew.iesi.metadata.configuration.action.trace.ActionParameterTraceConfiguration;
 import io.metadew.iesi.metadata.configuration.connection.ConnectionConfiguration;
 import io.metadew.iesi.metadata.definition.action.ActionParameter;
 import io.metadew.iesi.metadata.definition.action.design.ActionParameterDesignTrace;
 import io.metadew.iesi.metadata.definition.action.key.ActionParameterKey;
-import io.metadew.iesi.metadata.definition.action.trace.ActionParameterTrace;
-import io.metadew.iesi.metadata.definition.action.trace.key.ActionParameterTraceKey;
 import io.metadew.iesi.metadata.definition.connection.key.ConnectionKey;
 import io.metadew.iesi.metadata.service.action.ActionParameterTraceService;
 import io.metadew.iesi.script.action.ActionParameterResolvement;
@@ -45,7 +42,6 @@ import java.util.List;
 import java.util.Optional;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 
 @Log4j2
@@ -85,44 +81,7 @@ public class HttpExecuteRequest extends ActionTypeExecution {
         HttpComponent httpComponent;
         if (componentVersion == null) {
             httpComponent = HttpComponentService.getInstance().getAndTrace(convertHttpRequestName(getParameterResolvedValue(REQUEST_KEY)), getActionExecution(), REQUEST_KEY);
-            ActionParameterDesignTrace actionParameterDesignTrace = new ActionParameterDesignTrace(
-                    getExecutionControl().getRunId(),
-                    getActionExecution().getProcessId(),
-                    getActionExecution().getAction().getMetadataKey().getActionId(),
-                    new ActionParameter(
-                            new ActionParameterKey(
-                                    getScriptExecution().getScript().getMetadataKey().getScriptId(),
-                                    getScriptExecution().getScript().getMetadataKey().getScriptVersion(),
-                                    getActionExecution().getAction().getMetadataKey().getActionId(),
-                                    REQUEST_VERSION
-                            ),
-                            ""
-                    ));
-
-            if (!ActionParameterDesignTraceConfiguration.getInstance().get(actionParameterDesignTrace.getMetadataKey()).isPresent()) {
-                ActionParameterDesignTraceConfiguration.getInstance().insert(actionParameterDesignTrace);
-                ActionParameterTraceService.getInstance().trace(
-                        getActionExecution(),
-                        REQUEST_VERSION,
-                        new Text(httpComponent.getVersion().toString())
-                );
-            } else {
-                getActionParameterResolvements().removeIf(o -> o.getActionParameter().getMetadataKey().getParameterName().equals(REQUEST_VERSION));
-                getActionParameterResolvements().add(
-                        new ActionParameterResolvement(
-                                new ActionParameter(
-                                        new ActionParameterKey(
-                                                getScriptExecution().getScript().getMetadataKey().getScriptId(),
-                                                getScriptExecution().getScript().getMetadataKey().getScriptVersion(),
-                                                getActionExecution().getAction().getMetadataKey().getActionId(),
-                                                REQUEST_VERSION
-                                        ),
-                                        ""
-                                ),
-                                new Text(httpComponent.getVersion().toString())
-                        )
-                );
-            }
+            updateRequestVersionLog(httpComponent.getVersion());
         } else {
             httpComponent = HttpComponentService.getInstance().getAndTrace(convertHttpRequestName(getParameterResolvedValue(REQUEST_KEY)), getActionExecution(), REQUEST_KEY, componentVersion);
         }
@@ -421,6 +380,47 @@ public class HttpExecuteRequest extends ActionTypeExecution {
             log.warn(MessageFormat.format("Status code of response {0} is not member of success status codes (1XX, 2XX, 3XX).",
                     httpResponse.getStatusLine().getStatusCode()));
             getActionExecution().getActionControl().increaseErrorCount();
+        }
+    }
+
+    private void updateRequestVersionLog(Long componentVersion) {
+        ActionParameterDesignTrace actionParameterDesignTrace = new ActionParameterDesignTrace(
+                getExecutionControl().getRunId(),
+                getActionExecution().getProcessId(),
+                getActionExecution().getAction().getMetadataKey().getActionId(),
+                new ActionParameter(
+                        new ActionParameterKey(
+                                getScriptExecution().getScript().getMetadataKey().getScriptId(),
+                                getScriptExecution().getScript().getMetadataKey().getScriptVersion(),
+                                getActionExecution().getAction().getMetadataKey().getActionId(),
+                                REQUEST_VERSION
+                        ),
+                        ""
+                ));
+
+        if (!ActionParameterDesignTraceConfiguration.getInstance().get(actionParameterDesignTrace.getMetadataKey()).isPresent()) {
+            ActionParameterDesignTraceConfiguration.getInstance().insert(actionParameterDesignTrace);
+            ActionParameterTraceService.getInstance().trace(
+                    getActionExecution(),
+                    REQUEST_VERSION,
+                    new Text(componentVersion.toString())
+            );
+        } else {
+            getActionParameterResolvements().removeIf(o -> o.getActionParameter().getMetadataKey().getParameterName().equals(REQUEST_VERSION));
+            getActionParameterResolvements().add(
+                    new ActionParameterResolvement(
+                            new ActionParameter(
+                                    new ActionParameterKey(
+                                            getScriptExecution().getScript().getMetadataKey().getScriptId(),
+                                            getScriptExecution().getScript().getMetadataKey().getScriptVersion(),
+                                            getActionExecution().getAction().getMetadataKey().getActionId(),
+                                            REQUEST_VERSION
+                                    ),
+                                    ""
+                            ),
+                            new Text(componentVersion.toString())
+                    )
+            );
         }
     }
 

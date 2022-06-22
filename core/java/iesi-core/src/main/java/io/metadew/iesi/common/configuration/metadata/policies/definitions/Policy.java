bb69bff9ca3e9de7b1ec831873ada6872d0f4e90
HamzaaKhattabi <h.khattabi@accenture.com>
2022-06-22 08:53:33 +0200

    revert

--- core/java/iesi-core/src/main/java/io/metadew/iesi/common/configuration/metadata/policies/definitions/Policy.java
+++
@@ -1,5 +0,0 @@
-package io.metadew.iesi.common.configuration.metadata.policies.definitions;
-
-public interface Policy<T> {
-    boolean verify(T toVerify);
-}
--- core/java/iesi-rest-without-microservices/src/main/java/io/metadew/iesi/server/rest/configuration/security/providers/ldap/LdapServer.java
+++
@@ -1,9 +0,0 @@
-package io.metadew.iesi.server.rest.configuration.security.providers.ldap;
-
-import lombok.Data;
-
-@Data
-public class LdapServer {
-    private String url;
-    private String base;
-}

cce8ee2a303376e8e11a9733c8678bda356f2e17
HamzaaKhattabi <h.khattabi@accenture.com>
2022-07-19 17:00:56 +0200

    update shell script for running iesi on docker

--- docker/run.test.sh
+++ docker/run.test.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --rm --entrypoint bash -it -p 8080:8080 -v /home/hkhattabi/test_dev:/opt/iesi/logs --network my-network iesi
+docker run --rm --entrypoint bash -it -p 8080:8080 -v /home/hkhattabi/IESI/logs:/opt/iesi/logs --network my-network iesi

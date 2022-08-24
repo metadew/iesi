bd837e6aca0d17c834043e72fb7f11c83c76bcdf
HamzaaKhattabi <h.khattabi@accenture.com>
2022-08-24 12:17:28 +0200

    wip

--- core/java/iesi-rest-without-microservices/src/main/java/io/metadew/iesi/server/rest/configuration/security/jwt/AuthorizationServerConfiguration.java
+++ core/java/iesi-rest-without-microservices/src/main/java/io/metadew/iesi/server/rest/configuration/security/jwt/AuthorizationServerConfiguration.java
@@ -4,16 +4,16 @@ import io.metadew.iesi.server.rest.configuration.security.IesiUserDetailsManager
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.token.AuthorizationServerTokenServices;
-import org.springframework.security.oauth2.provider.token.DefaultAccessTokenConverter;
-import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
-import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.oauth2.provider.token.*;
 import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 
@@ -52,7 +52,16 @@ public class AuthorizationServerConfiguration extends AuthorizationServerConfigu
 
     @Bean
     public TokenStore tokenStore() {
-        return new JdbcTokenStore(dataSource);
+        final JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+        final AuthenticationKeyGenerator authenticationKeyGenerator = new DefaultAuthenticationKeyGenerator();
+        return new JdbcTokenStore(dataSource) {
+            @Override
+            public void storeAccessToken(final OAuth2AccessToken token, final OAuth2Authentication authentication) {
+                final String key = authenticationKeyGenerator.extractKey(authentication);
+                jdbcTemplate.update("delete from oauth_access_token where authentication_id = ?", key);
+                super.storeAccessToken(token, authentication);
+            }
+        };
     }
 
     @Bean

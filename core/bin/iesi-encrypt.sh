cbf018ef3232eced9574c32611327fa9a9002dfb
HamzaaKhattabi <h.khattabi@accenture.com>
2022-10-27 11:22:37 +0200

    adding plugin lib to classpath on encrypt & metadata shell script

--- core/bin/iesi-encrypt.sh
+++ core/bin/iesi-encrypt.sh
@@ -3,6 +3,7 @@ calling_dir=$(pwd)
 relative_dir=$(dirname "${BASH_SOURCE[0]}")
 absolute_dir=$calling_dir/$relative_dir
 lib_dir=$absolute_dir/../lib
+plugin_lib=$absolute_dir/../plugin_lib
 
 classpath="*"
 
@@ -10,6 +11,13 @@ cd $lib_dir
 for i in *.jar; do
     classpath="$classpath:$lib_dir/$i"
 done
+
+cd $plugin_lib
+for i in *.jar; do
+  classpath="$classpath:$plugin_lib/$i"
+done
+
+cd $lib_dir
 
 
 java -cp $classpath io.metadew.iesi.Application -launcher encryption "$@"
